fb2244325714476508a3e119e06fa2548d1721c1
Abhishek <abhishekk19@gmail.com>
2019-09-07 11:40:00 +0530

    3Sum closest.

---
+++ Medium/3Sum_closest/main.go
@@ -0,0 +1,76 @@
+// https://leetcode.com/problems/3sum-closest/
+
+package main
+
+import "sort"
+
+func equal(x, y []int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i, val := range x {
+		if val != y[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func in(arr []int, nested [][]int) (present bool) {
+	for i := 0; i < len(nested); i++ {
+		if equal(arr, nested[i]) {
+			return true
+		}
+	}
+	return false
+}
+
+func sum(x []int) (sum int) {
+	for _, v := range x {
+		sum += v
+	}
+	return sum
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -1 * x
+	}
+	return x
+}
+
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	l := len(nums)
+	actualSum := sum(nums[:3])
+	if l > 3 {
+		for i := 0; i < l-2; i++ {
+			j := i + 1
+			k := l - 1
+			for j < k {
+				currentSum := nums[i] + nums[j] + nums[k]
+				if min(abs(target-actualSum), abs(target-currentSum)) == abs(target-currentSum) {
+					actualSum = currentSum
+				}
+
+				if currentSum > target {
+					k--
+				} else if currentSum < target {
+					j++
+				} else {
+					return currentSum
+				}
+			}
+		}
+	}
+	return actualSum
+}
+
+func main() {}
