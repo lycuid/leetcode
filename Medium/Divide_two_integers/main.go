d3249b1fa37c544e1a7874117e3723f2f7f7750e
Abhishek <abhishekk19@gmail.com>
2019-11-15 23:32:55 +0530

    Divide two integers

---
+++ Medium/Divide_two_integers/main.go
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/divide-two-integers/
+
+package main
+
+import "math"
+
+func negate(i int) int {
+	return i - (i + i)
+}
+
+func multiply(i, j int) int {
+	if j == 0 {
+		return 0
+	}
+
+	if j > 0 {
+		return i + multiply(i, j-1)
+	}
+
+	return negate(multiply(i, negate(j)))
+}
+
+func signOf(i int) int {
+	if i < 0 {
+		return -1
+	}
+	return 1
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return negate(i)
+	}
+	return i
+}
+
+func divide(dividend int, divisor int) (res int) {
+	isNegative := multiply(signOf(dividend), signOf(divisor)) < 0
+	dividend = abs(dividend)
+	divisor = abs(divisor)
+	for dividend >= divisor {
+		dividend -= divisor
+		res++
+	}
+
+	if res > math.MaxInt32 {
+		res = math.MaxInt32
+	}
+
+	if isNegative {
+		return negate(res)
+	}
+	return res
+}
+
+func main() { }
+
