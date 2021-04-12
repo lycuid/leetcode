5fff576c05acea6dda859a2b73cf61a407ed353c
Abhishek Kadam <abhishekk19@gmail.com>
2021-04-12 21:52:22 +0530

    Rotate List.

---
+++ Medium/Rotate_list/main.go
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/rotate-list/
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func rotateRight(head *ListNode, k int) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	length := 0
+	for h := head; true; h = h.Next {
+		length++
+		if h.Next == nil {
+			h.Next = head
+			break
+		}
+	}
+	k %= length
+
+	newhead := head
+	for l := 0; l < length; l++ {
+		if l >= length-k-1 {
+			continue
+		}
+		newhead = newhead.Next
+	}
+	tmp := newhead
+	newhead = newhead.Next
+	tmp.Next = nil
+
+	return newhead
+}
+
+func main() {}
