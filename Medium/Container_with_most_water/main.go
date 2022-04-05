6f7efcbf3af9c615bd1c1c03897b5ed503feb53e
Abhishek Kadam <abhishekk19@gmail.com>
2022-04-05 14:28:18 +0530

    Container with most water (re-upload, part of daily challenge).

--- Medium/Container_with_most_water/main.go
+++ Medium/Container_with_most_water/main.go
@@ -1,6 +1,12 @@
 // https://leetcode.com/problems/container-with-most-water/
-
 package main
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
 
 func max(x, y int) int {
 	if x > y {
@@ -9,25 +15,17 @@ func max(x, y int) int {
 	return y
 }
 
-func min(x, y int) int {
-	if x <= y {
-		return x
-	}
-	return y
-}
-
-func maxArea(height []int) (maxSoFar int) {
-	left := 0
-	right := len(height) - 1
-	for left < right {
-		maxSoFar = max(min(height[left], height[right])*(right-left), maxSoFar)
-		if height[left] > height[right] {
-			right--
+func maxArea(height []int) (max_so_far int) {
+	l, r := 0, len(height)-1
+	for l < r {
+		max_so_far = max(min(height[l], height[r])*(r-l), max_so_far)
+		if height[l] > height[r] {
+			r--
 		} else {
-			left++
+			l++
 		}
 	}
-	return maxSoFar
+	return max_so_far
 }
 
 func main() {}
