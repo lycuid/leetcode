d3632dbb780511ef47caed4a2bd346322e08a5d4
Abhishek Kadam <abhishekk19@gmail.com>
2022-04-14 16:49:29 +0530

    Search in a binary search tree.

---
+++ Easy/Search_in_a_binary_search_tree/main.go
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/search-in-a-binary-search-tree/
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		goto EXIT
+	}
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	} else if root.Val < val {
+		return searchBST(root.Right, val)
+	}
+EXIT:
+	return root
+}
+
+func main() {}
