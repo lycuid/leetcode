7eebd98d112fb1da40ac1e6b9fa688c61ca3d80f
Abhishek Kadam <abhishekk19@gmail.com>
2022-05-30 12:14:46 +0530

    Divide two integers (re-done for daily challenge).

--- Medium/Divide_two_integers/main.go
+++ Medium/Divide_two_integers/main.go
@@ -1,59 +1,45 @@
 // https://leetcode.com/problems/divide-two-integers/
-
 package main
 
 import "math"
 
-func negate(i int) int {
-	return i - (i + i)
-}
-
-func multiply(i, j int) int {
-	if j == 0 {
-		return 0
-	}
-
-	if j > 0 {
-		return i + multiply(i, j-1)
-	}
-
-	return negate(multiply(i, negate(j)))
-}
-
-func signOf(i int) int {
-	if i < 0 {
+func SignOf(x int) int {
+	if Abs(x) != x {
 		return -1
 	}
 	return 1
 }
 
-func abs(i int) int {
-	if i < 0 {
-		return negate(i)
+func Abs(x int) int {
+	if x < 0 {
+		return -x
 	}
-	return i
+	return x
+}
+
+func MinMax(x, min, max int) int {
+	if min < x {
+		x = min
+	}
+	if x < max {
+		x = max
+	}
+	return x
+
 }
 
 func divide(dividend int, divisor int) (res int) {
-	isNegative := multiply(signOf(dividend), signOf(divisor)) < 0
-	dividend = abs(dividend)
-	divisor = abs(divisor)
-
-	for dividend >= divisor {
-		dividend -= divisor
-		res++
-	}
-
-	if isNegative {
-		res = negate(res)
-		if res < math.MinInt32 {
-			res = math.MinInt32
+	sign := SignOf(dividend) * SignOf(divisor)
+	dd, dv := Abs(dividend), Abs(divisor)
+	for dd >= dv {
+		lshift := 0
+		for dd > (dv << (lshift + 1)) {
+			lshift++
 		}
-	} else {
-		if res > math.MaxInt32 {
-			res = math.MaxInt32
-		}
+		dd -= (dv << lshift)
+		res += 1 << lshift
 	}
+	res = MinMax(sign*res, math.MaxInt32, math.MinInt32)
 	return res
 }
 
