d387389b6cf3267172ecbf90aada7dafc23bc533
Abhishek Kadam <abhishekk19@gmail.com>
2022-06-09 11:54:24 +0530

    Binary tree level order traversal.

---
+++ Medium/Binary_tree_level_order_traversal/main.go
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func Solve(root *TreeNode, depth int, ret *[][]int) {
+	if root != nil {
+		if len(*ret) < depth+1 {
+			*ret = append(*ret, []int{})
+		}
+		(*ret)[depth] = append((*ret)[depth], root.Val)
+		Solve(root.Left, depth+1, ret)
+		Solve(root.Right, depth+1, ret)
+	}
+}
+
+func levelOrder(root *TreeNode) (ret [][]int) {
+	Solve(root, 0, &ret)
+	return ret
+}
+
+func main() {}
