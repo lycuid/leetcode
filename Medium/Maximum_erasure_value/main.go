035af2c95d5264f42866d6c464bb5653f145307d
Abhishek Kadam <abhishekk19@gmail.com>
2022-06-12 13:36:40 +0530

    Maximum erasure value.

---
+++ Medium/Maximum_erasure_value/main.go
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/maximum-erasure-value/
+package main
+
+func maximumUniqueSubarray(nums []int) (res int) {
+	var cache [1e4 + 1]bool
+	for i, j, sum := 0, 0, 0; i < len(nums); i++ {
+		for ; cache[nums[i]] && j <= i; j++ {
+			sum, cache[nums[j]] = sum-nums[j], false
+		}
+		sum, cache[nums[i]] = sum+nums[i], true
+		if sum > res {
+			res = sum
+		}
+	}
+	return res
+}
+
+func main() {}
