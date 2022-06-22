37d869bee904478c229e166f499062cf39717831
Abhishek Kadam <abhishekk19@gmail.com>
2022-06-22 13:06:06 +0530

    Kth largest element in an array (refactored, using heap...for daily challenge).

--- Medium/Kth_largest_element_in_an_array/main.go
+++ Medium/Kth_largest_element_in_an_array/main.go
@@ -1,11 +1,59 @@
 // https://leetcode.com/problems/kth-largest-element-in-an-array/
 package main
 
-import "sort"
+func Heapify(root int, nums []int) {
+	if root*2 >= len(nums) {
+		return
+	}
+	child := root * 2
+	if root*2+1 < len(nums) && nums[child] < nums[root*2+1] {
+		child = root*2 + 1
+	}
+	if nums[root] < nums[child] {
+		nums[child], nums[root] = nums[root], nums[child]
+		Heapify(child, nums)
+	}
+}
+
+type PriorityQueue struct {
+	inner []int
+}
+
+func MakePQ(n []int) PriorityQueue {
+	inner := make([]int, len(n)+1)
+	for i, val := range n {
+		inner[i+1] = val
+	}
+	for i := len(inner) / 2; i > 0; i-- {
+		Heapify(i, inner)
+	}
+	return PriorityQueue{inner}
+}
+
+func (this *PriorityQueue) Pop() int {
+	item := this.inner[1]
+	this.inner[1] = this.inner[len(this.inner)-1]
+	this.inner = this.inner[:len(this.inner)-1]
+	for i := 1; i*2 < len(this.inner); {
+		j := i * 2
+		if i*2+1 < len(this.inner) && this.inner[i*2+1] > this.inner[i*2] {
+			j = i*2 + 1
+		}
+		if this.inner[i] > this.inner[j] {
+			break
+		}
+		this.inner[i], this.inner[j] = this.inner[j], this.inner[i]
+		i = j
+	}
+	return item
+}
 
 func findKthLargest(nums []int, k int) int {
-	sort.Ints(nums)
-	return nums[len(nums)-k]
+	pq := MakePQ(nums)
+	for ; k > 1; k-- {
+		pq.Pop()
+	}
+	return pq.Pop()
 }
 
 func main() {}
