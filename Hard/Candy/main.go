772e24869f80d87f8a903af8612bfc112694844a
Abhishek Kadam <abhishekk19@gmail.com>
2022-07-04 14:02:25 +0530

    Candy.

---
+++ Hard/Candy/main.go
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/candy/
+package main
+
+func candy(ratings []int) int {
+	n := len(ratings)
+	cache := make([]int, n)
+	for i := 1; i < n; i++ {
+		if ratings[i] > ratings[i-1] {
+			cache[i] = cache[i-1] + 1
+		}
+	}
+	ret := cache[n-1] + 1
+	for i := n - 2; i >= 0; i-- {
+		if ratings[i] > ratings[i+1] && cache[i] < cache[i+1]+1 {
+			cache[i] = cache[i+1] + 1
+		}
+		ret += cache[i] + 1
+	}
+	return ret
+}
+
+func main() {}
