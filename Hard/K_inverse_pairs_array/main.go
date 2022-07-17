277c3deea57241ffeaa50d08d9087487b0b3a34d
Abhishek Kadam <abhishekk19@gmail.com>
2022-07-18 00:08:23 +0530

    K inverse pairs array.

---
+++ Hard/K_inverse_pairs_array/main.go
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/k-inverse-pairs-array/
+package main
+
+const MOD = 1e9 + 7
+
+func kInversePairs(n int, k int) int {
+	cache := make([][]int, n+1)
+	for i := range cache {
+		cache[i] = make([]int, k+1)
+	}
+	cache[0][0] = 0
+	for i := 1; i <= n; i++ {
+		var tmp int
+		for j := 0; j <= k; j++ {
+			tmp += cache[i-1][j]
+			if j >= i {
+				tmp -= cache[i-1][j-i]
+			}
+			cache[i][j] = (cache[i][j] + tmp) % MOD
+		}
+	}
+	return cache[n][k]
+}
+
+func main() {}
