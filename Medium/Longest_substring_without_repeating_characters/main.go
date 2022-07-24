f82b408a0e1d805e9d4bc6b9cbc021ac77b005e0
Abhishek Kadam <abhishekk19@gmail.com>
2022-07-24 22:47:20 +0530

    Longest substring without repeating characters (refactored for practice).

--- Medium/Longest_substring_without_repeating_characters/main.go
+++ Medium/Longest_substring_without_repeating_characters/main.go
@@ -4,7 +4,7 @@ package main
 func lengthOfLongestSubstring(s string) (ret int) {
 	var cache [129]bool
 	for i, j := 0, 0; i < len(s); i++ {
-		for ; cache[s[i]] && j < i; j++ {
+		for ; cache[s[i]] && j <= i; j++ {
 			cache[s[j]] = false
 		}
 		if i-j+1 > ret {
