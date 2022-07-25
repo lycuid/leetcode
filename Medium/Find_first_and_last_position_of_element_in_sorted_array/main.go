a64e14863a28ffe794aa085a62bf2b8b8f42621a
Abhishek Kadam <abhishekk19@gmail.com>
2022-07-25 13:10:27 +0530

    Find first and last position of element in sorted array (refactored, for daily challenge).

--- Medium/Find_first_and_last_position_of_element_in_sorted_array/main.go
+++ Medium/Find_first_and_last_position_of_element_in_sorted_array/main.go
@@ -1,53 +1,41 @@
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
-
 package main
 
-func getBounds(nums []int, pivot, start, end int) []int {
-	lower, upper := pivot, pivot
-	target := nums[pivot]
-
-	if nums[start] == target {
-		lower = start
-	} else {
-		for start < lower-1 {
-			m := (start + lower) / 2
-			if nums[m] == target {
-				lower = m
-			} else {
-				start = m
-			}
-		}
-	}
-
-	if nums[end] == target {
-		upper = end
-	} else {
-		for end > upper+1 {
-			m := (end + upper) / 2
-			if nums[m] == target {
-				upper = m
-			} else {
-				end = m
-			}
-		}
-	}
-
-	return []int{lower, upper}
-}
-
 func searchRange(nums []int, target int) []int {
-	l := 0
-	r := len(nums) - 1
-
+	l, r := 0, len(nums)-1
 	for l <= r {
-		m := (l + r) / 2
-		n := nums[m]
-		if n < target {
-			l = m + 1
-		} else if n > target {
-			r = m - 1
+		mid := (l + r) / 2
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
 		} else {
-			return getBounds(nums, m, l, r)
+			left, lower, upper, right := 0, mid, mid, len(nums)-1
+			if nums[left] == target {
+				lower = left
+			} else {
+				for left+1 < lower {
+					mid := (left + lower) / 2
+					if nums[mid] == target {
+						lower = mid
+					} else {
+						left = mid
+					}
+				}
+			}
+			if nums[right] == target {
+				upper = right
+			} else {
+				for upper < right-1 {
+					mid := (upper + right) / 2
+					if nums[mid] == target {
+						upper = mid
+					} else {
+						right = mid
+					}
+				}
+			}
+			return []int{lower, upper}
 		}
 	}
 	return []int{-1, -1}
