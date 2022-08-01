d70f0c3f353d1ccdabcc6d728f970e578b87599e
Abhishek Kadam <abhishekk19@gmail.com>
2022-08-01 11:07:29 +0530

    Unique paths (refactored, for daily challenge).

--- Medium/Unique_paths/main.go
+++ Medium/Unique_paths/main.go
@@ -1,23 +1,18 @@
 // https://leetcode.com/problems/unique-paths/
 package main
 
-func uniquePaths(rows int, cols int) int {
-	dp := make([][]int, rows)
-	for i := range dp {
-		dp[i] = make([]int, cols)
+func uniquePaths(m int, n int) int {
+	cache := make([][]int, m+1)
+	for i := range cache {
+		cache[i] = make([]int, n+1)
 	}
-	for i := 0; i < rows; i++ {
-		dp[i][0] = 1
-	}
-	for i := 0; i < cols; i++ {
-		dp[0][i] = 1
-	}
-	for i := 1; i < rows; i++ {
-		for j := 1; j < cols; j++ {
-			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+	cache[1][1] = 1
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			cache[i][j] += (cache[i-1][j] + cache[i][j-1])
 		}
 	}
-	return dp[rows-1][cols-1]
+	return cache[m][n]
 }
 
 func main() {}
