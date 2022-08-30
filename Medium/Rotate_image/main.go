122fe417811e97f78e02d78da59c555b00ee16e0
Abhishek Kadam <abhishekk19@gmail.com>
2022-08-30 14:14:24 +0530

    Rotate image (refactored, for daily challenge).

--- Medium/Rotate_image/main.go
+++ Medium/Rotate_image/main.go
@@ -2,18 +2,11 @@
 package main
 
 func rotate(m [][]int) {
-	s, e := 0, len(m)-1
-	for e >= s {
-		for i := 0; i < e-s; i++ {
-			temp := m[s][s+i]
-			m[s][s+i] = m[e-i][s]
-			m[e-i][s] = m[e][e-i]
-			m[e][e-i] = m[s+i][e]
-			m[s+i][e] = temp
+	n := len(m) - 1
+	for i := 0; i <= n/2; i++ {
+		for j := i; j < n-i; j++ {
+			m[i][j], m[j][n-i], m[n-i][n-j], m[n-j][i] =
+				m[n-j][i], m[i][j], m[j][n-i], m[n-i][n-j]
 		}
-		s++
-		e--
 	}
 }
-
-func main() {}
