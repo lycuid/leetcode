4601212fb69b79b8c22362a292d82490d78bfe98
Abhishek Kadam <abhishekk19@gmail.com>
2022-10-07 22:29:41 +0530

    My calendar III.

---
+++ Hard/0732_My_calendar_iii/main.go
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/my-calendar-iii/description/
+package main
+
+import "sort"
+
+type MyCalendarThree struct {
+	events map[int]int
+}
+
+func Constructor() MyCalendarThree {
+	return MyCalendarThree{make(map[int]int)}
+}
+
+func (this *MyCalendarThree) Book(s int, e int) (k int) {
+	this.events[s], this.events[e] = this.events[s]+1, this.events[e]-1
+	current, keys := 0, make([]int, len(this.events))
+	for key := range this.events {
+		keys[current] = key
+		current++
+	}
+	sort.Ints(keys)
+	current = 0
+	for _, key := range keys {
+		if current += this.events[key]; current > k {
+			k = current
+		}
+	}
+	return k
+}
+
+func main() {}
