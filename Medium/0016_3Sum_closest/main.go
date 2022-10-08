309ac41ea0715e0b7047600227b0d66f1b8e0f65
Abhishek Kadam <abhishekk19@gmail.com>
2022-10-08 12:39:30 +0530

    3Sum closest (refactored, for daily challenge).

--- Medium/0016_3Sum_closest/main.go
+++ Medium/0016_3Sum_closest/main.go
@@ -1,76 +1,35 @@
 // https://leetcode.com/problems/3sum-closest/
-
 package main
 
 import "sort"
-
-func equal(x, y []int) bool {
-	if len(x) != len(y) {
-		return false
-	}
-	for i, val := range x {
-		if val != y[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func in(arr []int, nested [][]int) (present bool) {
-	for i := 0; i < len(nested); i++ {
-		if equal(arr, nested[i]) {
-			return true
-		}
-	}
-	return false
-}
-
-func sum(x []int) (sumVal int) {
-	for _, v := range x {
-		sumVal += v
-	}
-	return sumVal
-}
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
+import "math"
 
 func abs(x int) int {
 	if x < 0 {
-		return -1 * x
+		return -x
 	}
 	return x
 }
 
 func threeSumClosest(nums []int, target int) int {
 	sort.Ints(nums)
-	l := len(nums)
-	actualSum := sum(nums[:3])
-	if l > 3 {
-		for i := 0; i < l-2; i++ {
-			j := i + 1
-			k := l - 1
-			for j < k {
-				currentSum := nums[i] + nums[j] + nums[k]
-				if min(abs(target-actualSum), abs(target-currentSum)) == abs(target-currentSum) {
-					actualSum = currentSum
-				}
-
-				if currentSum > target {
-					k--
-				} else if currentSum < target {
-					j++
-				} else {
-					return currentSum
-				}
+	ret := math.MaxInt
+	for i := 0; i < len(nums)-2; i++ {
+		for j, k := i+1, len(nums)-1; j < k; {
+			sum := nums[i] + nums[j] + nums[k]
+			if abs(target-sum) < abs(target-ret) {
+				ret = sum
+			}
+			if sum < target {
+				j++
+			} else if sum > target {
+				k--
+			} else {
+				return sum
 			}
 		}
 	}
-	return actualSum
+	return ret
 }
 
 func main() {}
