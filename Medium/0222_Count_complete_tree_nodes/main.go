10451510f3324a038d5f9b55565bd3069131d7d0
Abhishek Kadam <abhishekk19@gmail.com>
2022-11-15 23:18:27 +0530

    Count complete tree nodes (refactored, for daily challenge).

--- Medium/0222_Count_complete_tree_nodes/main.go
+++ Medium/0222_Count_complete_tree_nodes/main.go
@@ -7,9 +7,19 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func countNodes(root *TreeNode) int {
+func countNodes(root *TreeNode) (ret int) {
 	if root == nil {
 		return 0
+	}
+	depthLeft, depthRight := 0, 0
+	for head := root; head != nil; head = head.Left {
+		depthLeft++
+	}
+	for head := root; head != nil; head = head.Right {
+		depthRight++
+	}
+	if depthLeft == depthRight {
+		return (1 << depthLeft) - 1
 	}
 	return 1 + countNodes(root.Left) + countNodes(root.Right)
 }
