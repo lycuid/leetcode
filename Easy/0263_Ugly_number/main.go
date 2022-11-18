3859f463b4c3dfb6a0a5ef5907adbd700fda60bc
Abhishek Kadam <abhishekk19@gmail.com>
2022-11-18 16:42:29 +0530

    Ugly number.

---
+++ Easy/0263_Ugly_number/main.go
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/ugly-number/
+package main
+
+func isUgly(n int) bool {
+	for n > 0 && n%2 == 0 {
+		n /= 2
+	}
+	for n > 0 && n%3 == 0 {
+		n /= 3
+	}
+	for n > 0 && n%5 == 0 {
+		n /= 5
+	}
+	return n == 1
+}
+
+func main() {}
