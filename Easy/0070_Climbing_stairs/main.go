c0deb2ff69ef9f183062d8a7911b1c4435edb2df
Abhishek Kadam <abhishekk19@gmail.com>
2022-12-12 12:15:48 +0530

    Climbing stairs (refactored, for daily challenge).

--- Easy/0070_Climbing_stairs/main.go
+++ Easy/0070_Climbing_stairs/main.go
@@ -1,17 +1,11 @@
 // https://leetcode.com/problems/climbing-stairs/
-
 package main
 
-func climbStairs(n int) int {
-  p := 0
-  p2 := 1
-  for n >= 2 {
-    p2 += p
-    p = p2 - p
-    n--
-  }
-  return p + p2
+func climbStairs(n int) (snd int) {
+	for fst := 1; n >= 0; n-- {
+		fst, snd = snd, fst+snd
+	}
+	return snd
 }
 
-func main() { }
-
+func main() {}
