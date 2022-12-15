b623f69153d824b991a591fd02e318de97f99acd
Abhishek Kadam <abhishekk19@gmail.com>
2022-12-15 13:09:20 +0530

    Longest common subsequence.

---
+++ Medium/1143_Longest_common_subsequence/main.go
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/longest-common-subsequence/
+package main
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	m, n := len(text1), len(text2)
+	cache := make([][]int, m+1)
+	for i := range cache {
+		cache[i] = make([]int, n+1)
+	}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if cache[i][j] = cache[i][j-1]; cache[i][j] < cache[i-1][j] {
+				cache[i][j] = cache[i-1][j]
+			}
+			if text1[i-1] == text2[j-1] {
+				cache[i][j] = cache[i-1][j-1] + 1
+			}
+		}
+	}
+	return cache[m][n]
+}
+
+func main() {}
