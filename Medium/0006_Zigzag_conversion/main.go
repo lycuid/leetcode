edf7a0288f520dde6cd57c6a3c1198801f1c8f86
Abhishek Kadam <abhishekk19@gmail.com>
2023-02-03 23:11:01 +0530

    Zigzag conversion (refactored, for daily challenge).

--- Medium/0006_Zigzag_conversion/main.go
+++ Medium/0006_Zigzag_conversion/main.go
@@ -1,19 +1,12 @@
 // https://leetcode.com/problems/zigzag-conversion/
-
 package main
-
-import (
-	"bytes"
-)
 
 func getDistances(n int) []int {
 	distances := make([]int, n)
 	distances[n-1] = 2*n - 2
-
 	for i := 0; i < n-1; i++ {
 		distances[i] = 2*n - (2 * (i + 1))
 	}
-
 	return distances
 }
 
@@ -21,25 +14,18 @@ func convert(s string, n int) string {
 	if n <= 1 {
 		return s
 	}
-
-	var zigZag bytes.Buffer
-
-	distances := getDistances(n)
-	dlen := len(distances)
-
-	for i := 0; i < dlen; i++ {
-		pointer := i
-		for j := 0; pointer < len(s); j++ {
-			zigZag.WriteByte(s[pointer])
+	ret, distances := make([]byte, len(s)), getDistances(n)
+	for i, n, x := 0, len(distances), 0; i < n; i++ {
+		for j, pointer := 0, i; pointer < len(s); j++ {
+			ret[x], x = s[pointer], x+1
 			if j%2 == 0 {
 				pointer += distances[i]
 			} else {
-				pointer += distances[dlen-i-1]
+				pointer += distances[n-i-1]
 			}
 		}
 	}
-
-	return zigZag.String()
+	return string(ret)
 }
 
 func main() {}
