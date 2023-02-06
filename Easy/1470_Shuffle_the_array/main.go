13960b6ff8e26ec3094cb02d82a00ff5ba3dbb21
Abhishek Kadam <abhishekk19@gmail.com>
2023-02-06 11:04:25 +0530

    Shuffle the array.

--- Easy/1470_Shuffle_the_array/main.go
+++ Easy/1470_Shuffle_the_array/main.go
@@ -1,10 +1,10 @@
 // https://leetcode.com/problems/shuffle-the-array/
 package main
 
-func shuffle(nums []int, n int) (ret []int) {
+func shuffle(nums []int, n int) []int {
+	ret := make([]int, 2*n)
 	for i := 0; i < n; i++ {
-		ret = append(ret, nums[i])
-		ret = append(ret, nums[n+i])
+		ret[i*2], ret[(i*2)+1] = nums[i], nums[i+n]
 	}
 	return ret
 }
