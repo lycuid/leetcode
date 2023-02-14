92812af7b683c814a9bec6f3b48a1e92dc6b496b
Abhishek Kadam <abhishekk19@gmail.com>
2023-02-14 12:48:01 +0530

    Add binary (refactored, for daily challenge).

--- Easy/0067_Add_binary/main.go
+++ Easy/0067_Add_binary/main.go
@@ -1,39 +1,31 @@
 // https://leetcode.com/problems/add-binary/
-
 package main
 
 func addBinary(a string, b string) string {
-  c := []byte{}
-
-  var carry byte = 0
-  var sub byte = 48
-  var base byte = 2
-
-  i, j := len(a) - 1, len(b) - 1
-  for i >= 0 && j >= 0 {
-    sum := a[i] + b[j] - (2 * sub) + carry
-    c = append([]byte{sum % base + sub}, c...)
-    carry = sum / base
-    i--
-    j--
-  }
-  for i >= 0 {
-    sum := a[i] - sub + carry
-    c = append([]byte{sum % base + sub}, c...)
-    carry = sum / base
-    i--
-  }
-  for j >= 0 {
-    sum := b[j] - sub + carry
-    c = append([]byte{sum % base + sub}, c...)
-    carry = sum / base
-    j--
-  }
-  if carry > 0 {
-    c = append([]byte{carry + 48}, c...)
-  }
-  return string(c)
+	n, m, i, j, carry := len(a)-1, len(b)-1, 0, 0, false
+	ret := make([]byte, n+m+2)
+	if n > m {
+		n, m, a, b = m, n, b, a
+	}
+	for num := 0; i <= n; i++ {
+		if num = int(a[n-i] + b[m-i] - '0' - '0'); carry {
+			num++
+		}
+		ret[j], j, carry = byte(num%2)+'0', j+1, num >= 2
+	}
+	for num := 0; i <= m; i++ {
+		if num = int(b[m-i] - '0'); carry {
+			num++
+		}
+		ret[j], j, carry = byte(num%2)+'0', j+1, num >= 2
+	}
+	if carry {
+		ret[j], j = '1', j+1
+	}
+	for i, n = 0, j; i < n/2; i++ {
+		ret[i], ret[n-1-i] = ret[n-1-i], ret[i]
+	}
+	return string(ret[:j])
 }
 
-func main() { }
-
+func main() {}
