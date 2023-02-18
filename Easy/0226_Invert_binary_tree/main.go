4a646572df64619e0819b6092a4f489f2d2b93fe
Abhishek Kadam <abhishekk19@gmail.com>
2023-02-18 10:50:11 +0530

    Invert binary tree.

---
+++ Easy/0226_Invert_binary_tree/main.go
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/invert-binary-tree/
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root != nil {
+		root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+	}
+	return root
+}
+
+func main() {}
