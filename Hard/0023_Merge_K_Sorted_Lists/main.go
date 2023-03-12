094c109a9f08dd2593d427b8c91c034b96abc569
Abhishek Kadam <abhishekk19@gmail.com>
2023-03-12 22:02:20 +0530

    Merge K sorted lists (refactored, for daily challenge).

--- Hard/0023_Merge_K_Sorted_Lists/main.go
+++ Hard/0023_Merge_K_Sorted_Lists/main.go
@@ -6,37 +6,51 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func mergeKLists(lists []*ListNode) *ListNode {
-	var (
-		head     *ListNode
-		previous *ListNode
-	)
-	l := len(lists)
+type PriorityQueue struct{ inner []*ListNode }
 
-	for {
-		smallest := -1
-		for i := 0; i < l; i++ {
-			current := lists[i]
-			if current == nil {
-				continue
-			}
-			if smallest == -1 || (*current).Val < lists[smallest].Val {
-				smallest = i
-			}
+func MakePQ() PriorityQueue            { return PriorityQueue{make([]*ListNode, 1)} }
+func (this PriorityQueue) Empty() bool { return len(this.inner) == 1 }
+
+func (this *PriorityQueue) Push(item *ListNode) {
+	if item != nil {
+		this.inner = append(this.inner, item)
+		for i := len(this.inner) - 1; i > 1 && this.inner[i].Val < this.inner[i/2].Val; i /= 2 {
+			this.inner[i], this.inner[i/2] = this.inner[i/2], this.inner[i]
 		}
-		if smallest == -1 { break; }
-		if previous == nil {
-			head = lists[smallest]
-			previous = lists[smallest]
-		} else {
-			previous.Next = lists[smallest]
-			previous = lists[smallest]
-		}
-		lists[smallest] = (*lists[smallest]).Next
 	}
+}
 
+func (this *PriorityQueue) Pop() *ListNode {
+	if this.Empty() {
+		return nil
+	}
+	item, n := this.inner[1], len(this.inner)-1
+	this.inner[1], this.inner = this.inner[n], this.inner[:n]
+	for i, j := 1, 2; j < n; i, j = j, j*2 {
+		if j+1 < n && this.inner[j+1].Val < this.inner[j].Val {
+			j++
+		}
+		if this.inner[i].Val < this.inner[j].Val {
+			break
+		}
+		this.inner[i], this.inner[j] = this.inner[j], this.inner[i]
+	}
+	return item
+}
+
+func mergeKLists(lists []*ListNode) (head *ListNode) {
+	pq := MakePQ()
+	for i := range lists {
+		pq.Push(lists[i])
+	}
+	if head = pq.Pop(); head != nil {
+		pq.Push(head.Next)
+	}
+	for last := head; !pq.Empty(); last = last.Next {
+		last.Next = pq.Pop()
+		pq.Push(last.Next.Next)
+	}
 	return head
 }
 
-func main() { }
-
+func main() {}
