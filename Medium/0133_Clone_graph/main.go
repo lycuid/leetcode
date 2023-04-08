899697394240d016667918c9fa49cedec0edb7b5
Abhishek Kadam <abhishekk19@gmail.com>
2023-04-08 14:10:18 +0530

    Clone graph (refactored, for daliy challenge).

--- Medium/0133_Clone_graph/main.go
+++ Medium/0133_Clone_graph/main.go
@@ -6,23 +6,24 @@ type Node struct {
 	Neighbors []*Node
 }
 
-func Clone(node *Node, cache *[101]*Node) *Node {
-	if node == nil {
-		return nil
+type Graph struct{ cache map[*Node]*Node }
+
+func (graph *Graph) Clone(node *Node) *Node {
+	if node != nil && graph.cache[node] == nil {
+		graph.cache[node] = &Node{Val: node.Val}
+		for _, n := range node.Neighbors {
+			graph.cache[node].Neighbors = append(
+				graph.cache[node].Neighbors,
+				graph.Clone(n),
+			)
+		}
 	}
-	if cache[node.Val] != nil {
-		return cache[node.Val]
-	}
-	cache[node.Val] = &Node{node.Val, make([]*Node, len(node.Neighbors))}
-	for i, neighbour := range node.Neighbors {
-		cache[node.Val].Neighbors[i] = Clone(neighbour, cache)
-	}
-	return cache[node.Val]
+	return graph.cache[node]
 }
 
 func cloneGraph(node *Node) *Node {
-	var cache [101]*Node
-	return Clone(node, &cache)
+	graph := Graph{make(map[*Node]*Node)}
+	return graph.Clone(node)
 }
 
 func main() {}
