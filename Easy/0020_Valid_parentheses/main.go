a38ac5784561a9e320d7e4655d7623723a0ecd3b
Abhishek Kadam <abhishekk19@gmail.com>
2023-04-10 11:13:20 +0530

    Valid parentheses (refactored, for daily challenge).

--- Easy/0020_Valid_parentheses/main.go
+++ Easy/0020_Valid_parentheses/main.go
@@ -1,47 +1,20 @@
 // https://leetcode.com/problems/valid-parentheses/
 package main
 
-type Stack struct {
-	inner  []rune
-	cursor int
-}
-
-func (this *Stack) Push(item rune) {
-	this.inner[this.cursor] = item
-	this.cursor++
-}
-
-func (this *Stack) Pop() (item rune) {
-	this.cursor--
-	item = this.inner[this.cursor]
-	return item
-}
-
-func (this *Stack) Empty() bool {
-	return this.cursor == 0
-}
-
-func isValid(str string) bool {
-	stack := Stack{make([]rune, len(str)), 0}
-	for _, ch := range str {
-		switch ch {
-		case 40, 91, 123:
-			stack.Push(ch)
-		case 41:
-			if stack.Empty() || stack.Pop() != 40 {
-				return false
-			}
-		case 93:
-			if stack.Empty() || stack.Pop() != 91 {
-				return false
-			}
-		case 125:
-			if stack.Empty() || stack.Pop() != 123 {
-				return false
-			}
+func isValid(s string) bool {
+	stack, cursor := make([]rune, len(s)), 0
+	for _, ch := range s {
+		if ch == '{' || ch == '[' || ch == '(' {
+			stack[cursor], cursor = ch, cursor+1
+		} else if cursor > 0 && ((ch == '}' && stack[cursor-1] == '{') ||
+			(ch == ']' && stack[cursor-1] == '[') ||
+			(ch == ')' && stack[cursor-1] == '(')) {
+			cursor--
+		} else {
+			return false
 		}
 	}
-	return stack.Empty()
+	return cursor == 0
 }
 
 func main() {}
