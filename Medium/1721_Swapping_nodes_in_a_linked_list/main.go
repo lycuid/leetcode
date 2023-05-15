a60800596c6ab8660aa83e46885903cc843753f4
Abhishek Kadam <abhishekk19@gmail.com>
2023-05-15 12:48:47 +0530

    Swapping nodes in a linked list (refactored, for daily challenge).

--- Medium/1721_Swapping_nodes_in_a_linked_list/main.go
+++ Medium/1721_Swapping_nodes_in_a_linked_list/main.go
@@ -6,40 +6,39 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func At(l *ListNode, i uint) *ListNode {
-	if i == 0 || l == nil {
-		return l
+type Tuple struct{ parent, child *ListNode }
+
+func Get(head *ListNode, k int) (ret Tuple) {
+	for ret.parent, ret.child = nil, head; k > 1; k-- {
+		ret.parent, ret.child = ret.child, ret.child.Next
 	}
-	return At(l.Next, i-1)
+	return ret
 }
 
-func swap(head *ListNode, x, y uint) *ListNode {
-	lx, ly := At(head, x), At(head, y)
-	lxp, lyp := head, head
-	for ; lxp != nil && lxp.Next != lx; lxp = lxp.Next {
-	}
-	for ; lyp != nil && lyp.Next != ly; lyp = lyp.Next {
-	}
-	if lxp != nil {
-		lxp.Next = ly
+func Swap(head *ListNode, p1, p2 Tuple) *ListNode {
+	if p1.parent != nil {
+		p1.parent.Next = p2.child
 	} else {
-		head = ly
+		head = p2.child
 	}
-	if lyp != nil {
-		lyp.Next = lx
+	if p2.parent != nil {
+		p2.parent.Next = p1.child
 	} else {
-		head = lx
+		head = p2.child
 	}
-	lx.Next, ly.Next = ly.Next, lx.Next
+	p1.child.Next, p2.child.Next = p2.child.Next, p1.child.Next
 	return head
 }
 
 func swapNodes(head *ListNode, k int) *ListNode {
-	count := 0
-	for l := head; l != nil; l = l.Next {
+	count := 1
+	for root := head; root != nil; root = root.Next {
 		count++
 	}
-	return swap(head, uint(k-1), uint(count-k))
+	if k > count/2 {
+		return Swap(head, Get(head, count-k), Get(head, k))
+	}
+	return Swap(head, Get(head, k), Get(head, count-k))
 }
 
 func main() {}
