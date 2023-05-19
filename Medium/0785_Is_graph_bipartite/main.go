645217f7a0396d7b460af365b2c91ba4d2f2f28f
Abhishek Kadam <abhishekk19@gmail.com>
2023-05-19 12:08:42 +0530

    Is graph bipartite (refactored, for daily challenge).

--- Medium/0785_Is_graph_bipartite/main.go
+++ Medium/0785_Is_graph_bipartite/main.go
@@ -1,39 +1,22 @@
 // https://leetcode.com/problems/is-graph-bipartite/
 package main
 
-const (
-	Orphan = iota
-	Group1 = iota
-	Group2 = iota
-)
-
-func TryPartition(parent int, children [][]int, group_of []int) bool {
-	new_group := Group1 + Group2 - group_of[parent]
-	for _, child := range children[parent] {
-		switch group_of[child] {
-		case Orphan:
-			group_of[child] = new_group
-		case group_of[parent]:
-			return false
-		default:
-			continue
-		}
-		if !TryPartition(child, children, group_of) {
-			return false
-		}
-	}
-	return true
-}
-
 func isBipartite(graph [][]int) bool {
-	group_of := make([]int, len(graph)) // 1 <= len(graph) <= 100
-	for node := range graph {
-		if group_of[node] != Orphan {
-			continue
-		}
-		group_of[node] = Group1
-		if !TryPartition(node, graph, group_of) {
-			return false
+	queue, set := []int{}, make([]int, len(graph))
+	for parent := range graph {
+		if set[parent] == 0 {
+			set[parent], queue = 1, append(queue, parent)
+			for root := 0; len(queue) > 0; {
+				root, queue = queue[0], queue[1:]
+				for _, child := range graph[root] {
+					if set[child] == set[root] {
+						return false
+					}
+					if set[child] == 0 {
+						queue, set[child] = append(queue, child), 3-set[root]
+					}
+				}
+			}
 		}
 	}
 	return true
