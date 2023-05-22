ac6447893c3238cd98bc20b907cf1320839cadad
Abhishek Kadam <abhishekk19@gmail.com>
2023-05-23 00:37:23 +0530

    Top K frequent elements (refactored, for daily challenge).

--- Medium/0347_Top_K_frequent_elements/main.go
+++ Medium/0347_Top_K_frequent_elements/main.go
@@ -1,29 +1,20 @@
 // https://leetcode.com/problems/top-k-frequent-elements/
 package main
 
-func Max(x, y int) int {
-	if x > y {
-		return x
-	}
-	return y
-}
-
 func topKFrequent(nums []int, k int) []int {
-	max_freq := nums[0]
-	freq := make(map[int]int) // a[n] = count
-	for _, val := range nums {
-		freq[val] += 1
-		max_freq = Max(max_freq, freq[val])
+	freq, ret, max := make(map[int]int), make([]int, k), 0
+	for _, num := range nums {
+		if freq[num]++; freq[num] > max {
+			max = freq[num]
+		}
 	}
-	weight_at := make([][]int, max_freq+1) // a[count] = {n1, n2, ..}
-	for n, count := range freq {
-		weight_at[count] = append(weight_at[count], n)
+	bucket := make([][]int, max+1)
+	for k, v := range freq {
+		bucket[v] = append(bucket[v], k)
 	}
-	ret := make([]int, k)
-	for i := max_freq; i >= 0 && k > 0; i-- {
-		for _, val := range weight_at[i] {
-			ret[k-1] = val
-			k--
+	for i := len(bucket) - 1; k > 0 && i >= 0; i-- {
+		for j := 0; k > 0 && j < len(bucket[i]); j++ {
+			ret[k-1], k = bucket[i][j], k-1
 		}
 	}
 	return ret
