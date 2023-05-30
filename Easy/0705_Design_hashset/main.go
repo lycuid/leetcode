1931e64d097a70a6343df7b61e774472706b779c
Abhishek Kadam <abhishekk19@gmail.com>
2023-05-30 23:36:21 +0530

    Design hashset (refactored, for daily challenge).

--- Easy/0705_Design_hashset/main.go
+++ Easy/0705_Design_hashset/main.go
@@ -1,24 +1,52 @@
 // https://leetcode.com/problems/design-hashset/
 package main
 
-type MyHashSet struct {
-	set []bool
+type Node struct {
+	Key  int
+	Next *Node
 }
 
+type MyHashSet struct{ inner []*Node }
+
 func Constructor() MyHashSet {
-	return MyHashSet{set: make([]bool, 1e6+1)}
+	return MyHashSet{inner: make([]*Node, 100)}
+}
+
+func (this MyHashSet) Hash(key int) int {
+	return key % len(this.inner)
 }
 
 func (this *MyHashSet) Add(key int) {
-	this.set[key] = true
+	if !this.Contains(key) {
+		this.inner[this.Hash(key)] = &Node{key, this.inner[this.Hash(key)]}
+	}
 }
 
 func (this *MyHashSet) Remove(key int) {
-	this.set[key] = false
+	var (
+		prev *Node = nil
+		curr *Node = this.inner[this.Hash(key)]
+	)
+	for curr != nil {
+		if curr.Key == key {
+			if prev == nil {
+				this.inner[this.Hash(key)] = curr.Next
+			} else {
+				prev.Next = curr.Next
+			}
+			curr.Next = nil
+		}
+		prev, curr = curr, curr.Next
+	}
 }
 
 func (this *MyHashSet) Contains(key int) bool {
-	return this.set[key]
+	for node := this.inner[this.Hash(key)]; node != nil; node = node.Next {
+		if node.Key == key {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {}
