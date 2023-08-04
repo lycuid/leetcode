3bb8686920f8da2596ba04279445197842f80d86
Abhishek Kadam <abhishekk19@gmail.com>
2023-08-05 02:05:08 +0530

    Word break (refactored, for daily challenge).

--- Medium/0139_Word_break/main.go
+++ Medium/0139_Word_break/main.go
@@ -2,12 +2,12 @@
 package main
 
 func wordBreak(s string, wordDict []string) bool {
-	cache, l := make([]bool, len(s)+1), len(s)
-	cache[l] = true
-	for i := len(s) - 1; i >= 0; i-- {
+	n, cache := len(s), make([]bool, len(s)+1)
+	cache[n] = true
+	for i := n - 1; i >= 0; i-- {
 		for j := 0; j < len(wordDict) && !cache[i]; j++ {
-			if wl := len(wordDict[j]); l-i >= wl && wordDict[j] == s[i:i+wl] {
-				cache[i] = cache[i+wl]
+			if l := len(wordDict[j]); i+l <= n {
+				cache[i] = wordDict[j] == s[i:i+l] && cache[i+l]
 			}
 		}
 	}
