bf5f8900ebbcb3543756afb3f573db670201cf7a
Abhishek Kadam <abhishekk19@gmail.com>
2023-08-12 16:45:29 +0530

    Unique paths ii.

--- Medium/0063_Unique_paths_ii/main.go
+++ Medium/0063_Unique_paths_ii/main.go
@@ -1,35 +1,34 @@
 // https://leetcode.com/problems/unique-paths-ii/
 package main
 
-func dfs(x, y int, unique_path [][]int) {
-	if unique_path[x][y] < 0 {
-		unique_path[x][y] = 0
-		if x < len(unique_path)-1 {
-			dfs(x+1, y, unique_path)
-			unique_path[x][y] += unique_path[x+1][y]
-		}
-		if y < len(unique_path[0])-1 {
-			dfs(x, y+1, unique_path)
-			unique_path[x][y] += unique_path[x][y+1]
+func uniquePathsWithObstacles(grid [][]int) int {
+	m, n := len(grid)-1, len(grid[0])-1
+	for i := 0; i <= m; i++ {
+		for j := 0; j <= n; j++ {
+			if grid[i][j] == 1 {
+				grid[i][j] = -1
+			}
 		}
 	}
-}
-
-func uniquePathsWithObstacles(graph [][]int) int {
-	n, m := len(graph), len(graph[0])
-	if graph[0][0]+graph[n-1][m-1] >= 1 {
-		return 0
+	if grid[0][0] != -1 {
+		grid[0][0] = 1
 	}
-	unique_path := make([][]int, n)
-	for i := range unique_path {
-		unique_path[i] = make([]int, m)
-		for j := range unique_path[i] {
-			unique_path[i][j] = graph[i][j] - 1
+	for i := 0; i <= m; i++ {
+		for j := 0; j <= n; j++ {
+			if grid[i][j] != -1 {
+				if i > 0 && grid[i-1][j] != -1 {
+					grid[i][j] += grid[i-1][j]
+				}
+				if j > 0 && grid[i][j-1] != -1 {
+					grid[i][j] += grid[i][j-1]
+				}
+			}
 		}
 	}
-	unique_path[n-1][m-1] = 1
-	dfs(0, 0, unique_path)
-	return unique_path[0][0]
+	if grid[m][n] == -1 {
+		grid[m][n] = 0
+	}
+	return grid[m][n]
 }
 
 func main() {}
