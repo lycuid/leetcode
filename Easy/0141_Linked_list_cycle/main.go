5fde803bb7427bda4583ecf3bbbaa86b1aa102e8
Abhishek Kadam <abhishekk19@gmail.com>
2023-09-04 14:09:46 +0530

    Linked list cycle.

---
+++ Easy/0141_Linked_list_cycle/main.go
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/linked-list-cycle/
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	slow, fast := head, head.Next
+	for slow != fast && fast != nil && fast.Next != nil {
+		slow, fast = slow.Next, fast.Next.Next
+	}
+	return slow == fast
+}
+
+func main() {}
