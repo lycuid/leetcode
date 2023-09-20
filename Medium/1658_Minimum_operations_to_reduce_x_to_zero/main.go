db6bd201a3e69e5c9c24ff87a49a2c528f59b2d4
Abhishek Kadam <abhishekk19@gmail.com>
2023-09-20 17:29:22 +0530

    Minimum operations to reduce x to zero.

--- Medium/1658_Minimum_operations_to_reduce_x_to_zero/main.go
+++ Medium/1658_Minimum_operations_to_reduce_x_to_zero/main.go
@@ -1,25 +1,24 @@
 // https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
 package main
 
-func minOperations(nums []int, x int) (res int) {
-	n, target, sum := len(nums), -x, 0
-	res = n + 1
-	for i := 0; i < n; i++ {
-		target += nums[i]
-	}
-	for i, j := 0, 0; i < n; i++ {
+func minOperations(nums []int, x int) int {
+	sum := 0
+	for i := range nums {
 		sum += nums[i]
-		for ; sum > target && j <= i; j++ {
-			sum -= nums[j]
+	}
+	sub, max, target := 0, -1, sum-x
+	for i, j := 0, 0; j < len(nums); {
+		for sub, j = sub+nums[j], j+1; i < j && sub > target; i++ {
+			sub -= nums[i]
 		}
-		if sum == target && res > n-(i-j+1) {
-			res = n - (i - j + 1)
+		if n := j - i; n > max && sub == target {
+			max = n
 		}
 	}
-	if res > n {
+	if max == -1 {
 		return -1
 	}
-	return res
+	return len(nums) - max
 }
 
 func main() {}
