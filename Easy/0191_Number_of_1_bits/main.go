50d747ac3d9541631453484563dcd3727d2234ad
Abhishek Kadam <abhishekk19@gmail.com>
2023-11-29 16:36:24 +0530

    Number of 1 bits (refactored, for daily challenge).

--- Easy/0191_Number_of_1_bits/main.go
+++ Easy/0191_Number_of_1_bits/main.go
@@ -1,11 +1,11 @@
 // https://leetcode.com/problems/number-of-1-bits/
 package main
 
-func hammingWeight(num uint32) (ones int) {
-	for i := 0; i < 32; i++ {
-		ones += int((num >> i) & 1)
+func hammingWeight(num uint32) (count int) {
+	for ; num > 0; num >>= 1 {
+		count += int(num & 1)
 	}
-	return ones
+	return count
 }
 
 func main() {}
