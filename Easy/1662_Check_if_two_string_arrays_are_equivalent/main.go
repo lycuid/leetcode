6c571e2aef8bcd2368e1f87db8bde8cf264d93ce
Abhishek Kadam <abhishekk19@gmail.com>
2023-12-01 12:50:11 +0530

    Check if two string arrays are equivalent (refactored, for daily challenge).

--- Easy/1662_Check_if_two_string_arrays_are_equivalent/main.go
+++ Easy/1662_Check_if_two_string_arrays_are_equivalent/main.go
@@ -2,18 +2,25 @@
 package main
 
 func arrayStringsAreEqual(word1 []string, word2 []string) bool {
-	cache := ""
-	for _, word := range word1 {
-		cache += word
-	}
-	for _, word := range word2 {
-		if n := len(word); len(cache) >= n && cache[:n] == word {
-			cache = cache[n:]
-		} else {
-			return false
+	var i, i1, j, j1 int
+	for i < len(word1) && j < len(word2) {
+		for ; i1 < len(word1[i]) && j1 < len(word2[j]); i1, j1 = i1+1, j1+1 {
+			if word1[i][i1] != word2[j][j1] {
+				return false
+			}
+		}
+		if i1 == len(word1[i]) {
+			if i++; i < len(word1) {
+				i1 = 0
+			}
+		}
+		if j1 == len(word2[j]) {
+			if j++; j < len(word2) {
+				j1 = 0
+			}
 		}
 	}
-	return len(cache) == 0
+	return i == len(word1) && i1 == len(word1[i-1]) && j == len(word2) && j1 == len(word2[j-1])
 }
 
 func main() {}
