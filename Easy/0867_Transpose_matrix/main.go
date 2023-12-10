d43892a5a5392a130604ef51789bf8a5ec5342c8
Abhishek Kadam <abhishekk19@gmail.com>
2023-12-10 15:11:10 +0530

    Transpose matrix (refactored, for daily challenge).

--- Easy/0867_Transpose_matrix/main.go
+++ Easy/0867_Transpose_matrix/main.go
@@ -1,12 +1,14 @@
 // https://leetcode.com/problems/transpose-matrix/
 package main
 
-func transpose(matrix [][]int) (ret [][]int) {
-	ret = make([][]int, len(matrix[0]))
-	for i := 0; i < len(matrix[0]); i++ {
+func transpose(matrix [][]int) [][]int {
+	ret := make([][]int, len(matrix[0]))
+	for i := range ret {
 		ret[i] = make([]int, len(matrix))
-		for j := 0; j < len(matrix); j++ {
-			ret[i][j] = matrix[j][i]
+	}
+	for i := range matrix {
+		for j := range matrix[i] {
+			ret[j][i] = matrix[i][j]
 		}
 	}
 	return ret
