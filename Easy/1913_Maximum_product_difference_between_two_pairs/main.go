6f8f5f7812eb724f53d744320fbb39a4138fa79f
Abhishek Kadam <abhishekk19@gmail.com>
2023-12-18 11:42:46 +0530

    Maximum product difference between two pairs.

---
+++ Easy/1913_Maximum_product_difference_between_two_pairs/main.go
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+package main
+
+import "sort"
+
+func maxProductDifference(nums []int) int {
+	sort.Ints(nums)
+	return nums[len(nums)-1]*nums[len(nums)-2] - nums[0]*nums[1]
+}
+
+func main() {}
