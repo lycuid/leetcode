68c4f65c7c09ce0114e02639af8f8e7bb5105609
Abhishek Kadam <abhishekk19@gmail.com>
2023-12-27 00:45:37 +0530

    Number of dice rolls with target sum (refactored, for daily challenge).

--- Medium/1155_Number_of_dice_rolls_with_target_sum/main.go
+++ Medium/1155_Number_of_dice_rolls_with_target_sum/main.go
@@ -4,16 +4,14 @@ package main
 func numRollsToTarget(n int, k int, target int) (ret int) {
 	cache := make([][]int, target+1)
 	for i := range cache {
-		cache[i] = make([]int, n+1)
-		if 0 < i && i <= k {
+		if cache[i] = make([]int, n+1); 0 < i && i <= k {
 			cache[i][1] = 1
 		}
 	}
-	for i := 1; i <= target; i++ {
+	for t := 1; t <= target; t++ {
 		for j := 2; j <= n; j++ {
-			for p := 1; p <= i && p <= k; p++ {
-				cache[i][j] += cache[i-p][j-1]
-				cache[i][j] %= (1e9 + 7)
+			for roll := 1; roll <= t && roll <= k; roll++ {
+				cache[t][j] = (cache[t][j] + cache[t-roll][j-1]) % (1e9 + 7)
 			}
 		}
 	}
