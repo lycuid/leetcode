6b462d8796bb159a78847ca8823158aa83790a7f
Abhishek Kadam <abhishekk19@gmail.com>
2023-12-27 13:51:07 +0530

    Minimum time to make rope colorful (refactored, for daily challenge).

--- Medium/1578_Minimum_time_to_make_rope_colorful/main.go
+++ Medium/1578_Minimum_time_to_make_rope_colorful/main.go
@@ -1,19 +1,16 @@
 // https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
 package main
 
-func minCost(cs string, time []int) (ret int) {
-	for i, j, max, sum := 0, 0, 0, 0; i < len(cs); i++ {
-		max, sum = time[i], time[i]
-		for j = i + 1; j < len(cs) && cs[i] == cs[j]; i, j = i+1, j+1 {
-			if sum += time[j]; time[j] > max {
-				max = time[j]
+func minCost(colors string, neededTime []int) (cost int) {
+	for i, j := 0, 1; j < len(colors); i, j = i+1, j+1 {
+		if colors[i] == colors[j] {
+			if neededTime[i] > neededTime[j] {
+				neededTime[i], neededTime[j] = neededTime[j], neededTime[i]
 			}
-		}
-		if sum != max { // more than 1 repeat (1 <= time[i] <= 1e4)
-			ret += sum - max
+			cost += neededTime[i]
 		}
 	}
-	return ret
+	return cost
 }
 
 func main() {}
