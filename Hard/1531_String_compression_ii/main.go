a23525d3e78615c37e7c10b8172cd8cbceae7ab1
Abhishek Kadam <abhishekk19@gmail.com>
2023-12-29 00:49:55 +0530

    String compression ii (refactored, for daily challenge).

--- Hard/1531_String_compression_ii/main.go
+++ Hard/1531_String_compression_ii/main.go
@@ -1,15 +1,25 @@
 // https://leetcode.com/problems/string-compression-ii/
 package main
 
+func digit_count(num int) (n int) {
+	if num > 1 {
+		for ; num > 0; num /= 10 {
+			n++
+		}
+	}
+	return n
+}
+
 func getLengthOfOptimalCompression(s string, k int) int {
-	var cache [110][110]int
-	n := len(s)
+	cache, n := make([][]int, len(s)+1), len(s)
 	for i := range cache {
+		cache[i] = make([]int, k+1)
 		for j := range cache[i] {
-			cache[i][j] = 1e5
+			cache[i][j] = 1e9 + 7
 		}
 	}
 	cache[0][0] = 0
+
 	for i := 1; i <= n; i++ {
 		for j := 0; j <= k; j++ {
 			var count, del int
@@ -20,15 +30,7 @@ func getLengthOfOptimalCompression(s string, k int) int {
 					del++
 				}
 				if j-del >= 0 {
-					d := 0
-					if count >= 100 {
-						d = 3
-					} else if count >= 10 {
-						d = 2
-					} else if count >= 2 {
-						d = 1
-					}
-					if m := cache[l-1][j-del] + 1 + d; m < cache[i][j] {
+					if m := cache[l-1][j-del] + 1 + digit_count(count); m < cache[i][j] {
 						cache[i][j] = m
 					}
 				}
