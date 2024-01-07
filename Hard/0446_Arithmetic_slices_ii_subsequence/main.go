f5d70faf5e6014a3a6d0c5b6bdbf528848d435a2
Abhishek Kadam <abhishekk19@gmail.com>
2024-01-07 23:39:33 +0530

    Arithmetic slices II - Subsequence (refactored, for daily challenge).

--- Hard/0446_Arithmetic_slices_ii_subsequence/main.go
+++ Hard/0446_Arithmetic_slices_ii_subsequence/main.go
@@ -1,19 +1,19 @@
 // https://leetcode.com/problems/arithmetic-slices-ii-subsequence/
 package main
 
-func numberOfArithmeticSlices(nums []int) (ret int) {
+func numberOfArithmeticSlices(nums []int) (count int) {
 	cache := make([]map[int]int, len(nums))
 	for i := range cache {
 		cache[i] = make(map[int]int)
 	}
 	for i := 0; i < len(nums); i++ {
-		for j := 0; j < i; j++ {
+		for j := i - 1; j >= 0; j-- {
 			diff := nums[i] - nums[j]
-			cache[i][diff] += (cache[j][diff] + 1)
-			ret += cache[j][diff]
+			cache[i][diff] += 1 + cache[j][diff]
+			count += cache[j][diff]
 		}
 	}
-	return ret
+	return count
 }
 
 func main() {}
