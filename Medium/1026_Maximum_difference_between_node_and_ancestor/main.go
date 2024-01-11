875914090e272e32f41e4b7abcd7c80eb7b04c07
Abhishek Kadam <abhishekk19@gmail.com>
2024-01-11 15:59:38 +0530

    Maximum difference between node and ancestor.

--- Medium/1026_Maximum_difference_between_node_and_ancestor/main.go
+++ Medium/1026_Maximum_difference_between_node_and_ancestor/main.go
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
 package main
 
+import "math"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -24,11 +26,13 @@ func Max(i, j int) int {
 func Aux(root *TreeNode, min, max int) (ret int) {
 	if root != nil {
 		min, max = Min(min, root.Val), Max(max, root.Val)
-		ret = Max(Aux(root.Right, min, max), Aux(root.Left, min, max))
+		ret = Max(Aux(root.Left, min, max), Aux(root.Right, min, max))
 	}
 	return Max(ret, max-min)
 }
 
-func maxAncestorDiff(root *TreeNode) int { return Aux(root, 1e5+9, 0) }
+func maxAncestorDiff(root *TreeNode) int {
+	return Aux(root, math.MaxInt, 0)
+}
 
 func main() {}
