b77d66618cd93e27ec2fa5237a0867c8f009b4b8
Abhishek Kadam <abhishekk19@gmail.com>
2024-01-28 03:10:10 +0530

    K inverse pairs array (refactored, for daily challenge).

--- Hard/0629_K_inverse_pairs_array/main.go
+++ Hard/0629_K_inverse_pairs_array/main.go
@@ -1,8 +1,6 @@
 // https://leetcode.com/problems/k-inverse-pairs-array/
 package main
 
-const MOD = 1e9 + 7
-
 func kInversePairs(n int, k int) int {
 	cache := make([][]int, n+1)
 	for i := range cache {
@@ -10,13 +8,11 @@ func kInversePairs(n int, k int) int {
 	}
 	cache[0][0] = 1
 	for i := 1; i <= n; i++ {
-		var tmp int
-		for j := 0; j <= k; j++ {
-			tmp += cache[i-1][j]
-			if j >= i {
+		for j, tmp := 0, 0; j <= k; j++ {
+			if tmp += cache[i-1][j]; j >= i {
 				tmp -= cache[i-1][j-i]
 			}
-			cache[i][j] = (cache[i][j] + tmp) % MOD
+			cache[i][j] = tmp % (1e9 + 7)
 		}
 	}
 	return cache[n][k]
