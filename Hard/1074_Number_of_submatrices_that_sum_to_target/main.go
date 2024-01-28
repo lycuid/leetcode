432d210b9f763da2bd378cc7776318d9b11cf441
Abhishek Kadam <abhishekk19@gmail.com>
2024-01-28 13:41:48 +0530

    Number of submatrices that sum to target (refactored, for daily challenge).

--- Hard/1074_Number_of_submatrices_that_sum_to_target/main.go
+++ Hard/1074_Number_of_submatrices_that_sum_to_target/main.go
@@ -1,49 +1,28 @@
 // https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/
 package main
 
-func numSubmatrixSumTarget(grid [][]int, target int) (ret int) {
-	rows, cols := len(grid), len(grid[0])
-	sum := make([][]int, rows)
-	for i := range sum {
-		sum[i] = make([]int, cols)
-		for j := range sum[i] {
-			sum[i][j] = grid[i][j]
-			if i > 0 {
-				sum[i][j] += sum[i-1][j]
-			}
-			if j > 0 {
-				sum[i][j] += sum[i][j-1]
-			}
-			if i > 0 && j > 0 {
-				sum[i][j] -= sum[i-1][j-1]
-			}
+func numSubmatrixSumTarget(matrix [][]int, target int) (count int) {
+	cache := make([][]int, len(matrix)+1)
+	for i := range cache {
+		cache[i] = make([]int, len(matrix[0])+1)
+	}
+	for i := 1; i < len(cache); i++ {
+		for j := 1; j < len(cache[i]); j++ {
+			cache[i][j] = matrix[i-1][j-1] + cache[i-1][j] + cache[i][j-1] - cache[i-1][j-1]
 		}
 	}
-	MatrixSum := func(x1, y1, x2, y2 int) (ans int) {
-		ans = sum[x2][y2]
-		if x1 > 0 {
-			ans -= sum[x1-1][y2]
-		}
-		if y1 > 0 {
-			ans -= sum[x2][y1-1]
-		}
-		if x1 > 0 && y1 > 0 {
-			ans += sum[x1-1][y1-1]
-		}
-		return ans
-	}
-	for x1 := 0; x1 < rows; x1++ {
-		for x2 := x1; x2 < rows; x2++ {
-			for y1 := 0; y1 < cols; y1++ {
-				for y2 := y1; y2 < cols; y2++ {
-					if MatrixSum(x1, y1, x2, y2) == target {
-						ret++
+	for x1 := 1; x1 < len(cache); x1++ {
+		for y1 := 1; y1 < len(cache[x1]); y1++ {
+			for x2 := x1; x2 < len(cache); x2++ {
+				for y2 := y1; y2 < len(cache[x2]); y2++ {
+					if cache[x2][y2]-cache[x1-1][y2]-cache[x2][y1-1]+cache[x1-1][y1-1] == target {
+						count++
 					}
 				}
 			}
 		}
 	}
-	return ret
+	return count
 }
 
 func main() {}
