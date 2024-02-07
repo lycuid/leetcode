6e875a990a463d8ac0707757ef9d304be489f4de
Abhishek Kadam <abhishekk19@gmail.com>
2024-02-07 15:09:28 +0530

    Sort characters by frequency (refactored, for daily challenge).

--- Medium/0451_Sort_characters_by_frequency/main.go
+++ Medium/0451_Sort_characters_by_frequency/main.go
@@ -1,28 +1,21 @@
 // https://leetcode.com/problems/sort-characters-by-frequency/
 package main
 
-func frequencySort(s string) (ret string) {
+import "sort"
+
+func frequencySort(s string) string {
 	var freq [128]int
-	var max_freq int
 	for _, ch := range s {
-		if freq[ch]++; freq[ch] >= max_freq {
-			max_freq = freq[ch] + 1
-		}
+		freq[ch]++
 	}
-	bytes_count := make([][]byte, max_freq+1)
-	for ch, f := range freq {
-		if f > 0 {
-			bytes_count[f] = append(bytes_count[f], byte(ch))
+	str := []byte(s)
+	sort.Slice(str, func(i, j int) bool {
+		if freq[str[i]] == freq[str[j]] {
+			return str[i] < str[j]
 		}
-	}
-	for i := max_freq; i >= 0; i-- {
-		for _, ch := range bytes_count[i] {
-			for j := 0; j < i; j++ {
-				ret += string(ch)
-			}
-		}
-	}
-	return ret
+		return freq[str[i]] > freq[str[j]]
+	})
+	return string(str)
 }
 
 func main() {}
