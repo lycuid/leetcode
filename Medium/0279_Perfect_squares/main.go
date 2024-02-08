da70671de19e63bea9f061411615aa404a152384
Abhishek Kadam <abhishekk19@gmail.com>
2024-02-08 15:53:56 +0530

    Perfect squares (refactored, for daily challenge).

--- Medium/0279_Perfect_squares/main.go
+++ Medium/0279_Perfect_squares/main.go
@@ -1,38 +1,19 @@
 // https://leetcode.com/problems/perfect-squares/
 package main
 
-const UNREACHABLE, UNVISITED int = 1e5 + 7, -1
-
-func Find(n int, squares []int, cache []int) int {
-	if l := 0; cache[n] == UNVISITED {
-		for r := len(squares) - 1; l < r; {
-			if mid := (l + r) / 2; squares[mid] <= n {
-				l = mid + 1
-			} else if squares[mid] > n {
-				r = mid
-			}
-		}
-		for cache[n] = UNREACHABLE; l >= 0; l-- {
-			if m := n - squares[l]; m >= 0 {
-				if cache[m] = Find(m, squares, cache); cache[m]+1 < cache[n] {
-					cache[n] = cache[m] + 1
-				}
+func numSquares(n int) int {
+	cache := []int{0}
+	for i := len(cache); i <= n; i++ {
+		cache = append(cache, i)
+		for j := i - 1; j >= i/2; j-- {
+			if k := i - j; k*k == i {
+				cache[i] = 1
+			} else if m := cache[j] + cache[k]; m < cache[i] {
+				cache[i] = m
 			}
 		}
 	}
 	return cache[n]
-}
-
-func numSquares(n int) int {
-	cache := make([]int, n+1)
-	for i := 1; i < len(cache); i++ {
-		cache[i] = UNVISITED
-	}
-	var squares []int
-	for i := 1; i*i <= n; i++ {
-		squares, cache[i*i] = append(squares, i*i), 1
-	}
-	return Find(n, squares, cache)
 }
 
 func main() {}
