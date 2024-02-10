98d99446ba138bfb0feb3ee24a30097fc2edacce
Abhishek Kadam <abhishekk19@gmail.com>
2024-02-10 15:39:32 +0530

    Palindromic substrings (refactored, for daily challenge).

--- Medium/0647_Palindromic_substrings/main.go
+++ Medium/0647_Palindromic_substrings/main.go
@@ -1,16 +1,16 @@
 // https://leetcode.com/problems/palindromic-substrings/
 package main
 
-func countSubstrings(str string) (count int) {
-	for c := range str {
-		for _, dj := range []int{0, 1} {
-			i, j := c, c+dj
-			for i >= 0 && j < len(str) && str[i] == str[j] {
-				count++
-				i--
-				j++
-			}
-		}
+func Count(s string, i, j int) (count int) {
+	for ; i >= 0 && j < len(s) && s[i] == s[j]; i, j = i-1, j+1 {
+		count++
+	}
+	return count
+}
+
+func countSubstrings(s string) (count int) {
+	for i := range s {
+		count += Count(s, i, i) + Count(s, i-1, i)
 	}
 	return count
 }
