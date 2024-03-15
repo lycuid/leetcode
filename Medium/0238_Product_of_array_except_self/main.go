0664e34f687582eb474f8ceddef0959a89e5210e
Abhishek Kadam <abhishekk19@gmail.com>
2024-03-15 14:27:48 +0530

    Product of array except self (refactored, for daily challenge).

--- Medium/0238_Product_of_array_except_self/main.go
+++ Medium/0238_Product_of_array_except_self/main.go
@@ -2,16 +2,15 @@
 package main
 
 func productExceptSelf(nums []int) []int {
-	partial, result := make([]int, len(nums)+1), make([]int, len(nums))
-	partial[len(nums)] = 1
-	for i := len(nums) - 1; i >= 0; i-- {
-		partial[i] = partial[i+1] * nums[i]
+	cache, n := make([]int, len(nums)+1), len(nums)
+	cache[n] = 1
+	for i := n - 1; i >= 0; i-- {
+		cache[i] = nums[i] * cache[i+1]
 	}
-	for i, product := 0, 1; i < len(result); i++ {
-		result[i] = product * partial[i+1]
-		product *= nums[i]
+	for i, prod := 0, 1; i < n; i++ {
+		nums[i], prod = prod*cache[i+1], prod*nums[i]
 	}
-	return result
+	return nums
 }
 
 func main() {}
