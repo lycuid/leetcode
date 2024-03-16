bb5d534c2291dda1d569a517ca0921b44cf39932
Abhishek Kadam <abhishekk19@gmail.com>
2024-03-16 18:10:53 +0530

    Contiguous array (refactored, for daily challenge).

--- Medium/0525_Contiguous_array/main.go
+++ Medium/0525_Contiguous_array/main.go
@@ -2,17 +2,17 @@
 package main
 
 func findMaxLength(nums []int) (ret int) {
-	index, sum := map[int]int{0: -1}, 0
+	cache, sum := map[int]int{0: -1}, 0
 	for i, num := range nums {
 		if sum++; num == 0 {
 			sum -= 2
 		}
-		if j, found := index[sum]; found {
+		if j, found := cache[sum]; found {
 			if i-j > ret {
 				ret = i - j
 			}
 		} else {
-			index[sum] = i
+			cache[sum] = i
 		}
 	}
 	return ret
