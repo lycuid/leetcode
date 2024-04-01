4ed0700507d591b5defb823e3fb691cfdc67f3cf
Abhishek Kadam <abhishekk19@gmail.com>
2024-04-01 13:53:16 +0530

    Length of last word (refactored, for daily challenge).

--- Easy/0058_Length_of_last_word/main.go
+++ Easy/0058_Length_of_last_word/main.go
@@ -1,18 +1,14 @@
 // https://leetcode.com/problems/length-of-last-word/
-
 package main
 
-import "strings"
-
-func lengthOfLastWord(s string) (l int) {
-  s = strings.TrimRight(s, " ")
-  i := len(s) - 1
-  for i >= 0 && s[i] != ' ' {
-    l++
-    i--
-  }
-  return l
+func lengthOfLastWord(s string) (size int) {
+	i := len(s) - 1
+	for ; i >= 0 && s[i] == ' '; i-- {
+	}
+	for ; i >= 0 && s[i] != ' '; i-- {
+		size++
+	}
+	return size
 }
 
-func main() { }
-
+func main() {}
