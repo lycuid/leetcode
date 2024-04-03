b808dfd8e14382f73166ca5d8c7555df70da4c64
Abhishek Kadam <abhishekk19@gmail.com>
2024-04-03 16:09:00 +0530

    Word search (refactored, for daily challenge).

--- Medium/0079_Word_search/main.go
+++ Medium/0079_Word_search/main.go
@@ -1,27 +1,27 @@
 // https://leetcode.com/problems/word-search/
 package main
 
-func Aux(y, x int, word string, grid [][]byte) bool {
+func Aux(y, x int, word string, board [][]byte) bool {
 	if len(word) == 1 {
 		return true
 	}
-	grid[y][x] = '#'
-	for _, d := range [][]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}} {
+	board[y][x] = '#'
+	for _, d := range [4][2]int{{0, 1}, {1, 0}, {-1, 0}, {0, -1}} {
 		dy, dx := y+d[0], x+d[1]
-		if dy >= 0 && dy < len(grid) && dx >= 0 && dx < len(grid[0]) {
-			if grid[dy][dx] == word[1] && Aux(dy, dx, word[1:], grid) {
+		if dy >= 0 && dy < len(board) && dx >= 0 && dx < len(board[0]) {
+			if word[1] == board[dy][dx] && Aux(dy, dx, word[1:], board) {
 				return true
 			}
 		}
 	}
-	grid[y][x] = word[0]
+	board[y][x] = word[0]
 	return false
 }
 
 func exist(board [][]byte, word string) bool {
-	for i := range board {
-		for j := range board[i] {
-			if board[i][j] == word[0] && Aux(i, j, word, board) {
+	for y := range board {
+		for x := range board[y] {
+			if word[0] == board[y][x] && Aux(y, x, word, board) {
 				return true
 			}
 		}
