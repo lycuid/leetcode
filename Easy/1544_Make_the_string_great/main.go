e6d04b30febd64ce286710832eef654f9e6bc739
Abhishek Kadam <abhishekk19@gmail.com>
2024-04-05 15:29:58 +0530

    Make the string great (refactored, for daily challenge).

--- Easy/1544_Make_the_string_great/main.go
+++ Easy/1544_Make_the_string_great/main.go
@@ -2,15 +2,21 @@
 package main
 
 func makeGood(s string) string {
-	var q []byte
-	for i := range s {
-		if n := len(q); n > 0 && (q[n-1] == s[i]-32 || q[n-1]-32 == s[i]) {
-			q = q[:n-1]
+	ret, i := []byte(s), 0
+	Abs := func(n int) int {
+		if n < 0 {
+			return -n
+		}
+		return n
+	}
+	for j := 0; j < len(s); j++ {
+		if i > 0 && Abs(int(ret[j])-int(ret[i-1])) == 32 {
+			i--
 		} else {
-			q = append(q, s[i])
+			ret[i], i = ret[j], i+1
 		}
 	}
-	return string(q)
+	return string(ret[:i])
 }
 
 func main() {}
