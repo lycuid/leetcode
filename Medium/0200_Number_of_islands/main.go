1667fd1998a37e0ed461dab2a34c96813957d575
Abhishek Kadam <abhishekk19@gmail.com>
2024-04-20 00:29:13 +0530

    Number of islands (refactored, for daily challenge).

--- Medium/0200_Number_of_islands/main.go
+++ Medium/0200_Number_of_islands/main.go
@@ -1,38 +1,32 @@
 // https://leetcode.com/problems/number-of-islands/
 package main
 
-const ISLAND = '1'
-
-func Dfs(y, x, id int, grid [][]byte, visited [][]bool) {
-	grid[y][x], visited[y][x] = byte(id), true
-	if y > 0 && grid[y-1][x] == ISLAND && !visited[y-1][x] {
-		Dfs(y-1, x, id, grid, visited)
+func Aux(grid [][]byte, i, j int) {
+	grid[i][j] = '0'
+	if i > 0 && grid[i-1][j] == '1' {
+		Aux(grid, i-1, j)
 	}
-	if x > 0 && grid[y][x-1] == ISLAND && !visited[y][x-1] {
-		Dfs(y, x-1, id, grid, visited)
+	if i < len(grid)-1 && grid[i+1][j] == '1' {
+		Aux(grid, i+1, j)
 	}
-	if y < len(grid)-1 && grid[y+1][x] == ISLAND && !visited[y+1][x] {
-		Dfs(y+1, x, id, grid, visited)
+	if j > 0 && grid[i][j-1] == '1' {
+		Aux(grid, i, j-1)
 	}
-	if x < len(grid[y])-1 && grid[y][x+1] == ISLAND && !visited[y][x+1] {
-		Dfs(y, x+1, id, grid, visited)
+	if j < len(grid[i])-1 && grid[i][j+1] == '1' {
+		Aux(grid, i, j+1)
 	}
 }
 
-func numIslands(grid [][]byte) (id int) {
-	visited := make([][]bool, len(grid))
-	for i := range visited {
-		visited[i] = make([]bool, len(grid[0]))
-	}
-	for y := range grid {
-		for x := range grid[y] {
-			if grid[y][x] == ISLAND && !visited[y][x] {
-				id--
-				Dfs(y, x, id, grid, visited)
+func numIslands(grid [][]byte) (count int) {
+	for i := range grid {
+		for j := range grid[i] {
+			if grid[i][j] == '1' {
+				count++
+				Aux(grid, i, j)
 			}
 		}
 	}
-	return -id
+	return count
 }
 
 func main() {}
