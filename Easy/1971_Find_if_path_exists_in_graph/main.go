864f2b619f23bacdc13f8636c5b552956feeb034
Abhishek Kadam <abhishekk19@gmail.com>
2024-04-21 17:42:01 +0530

    Find if path exists in graph (refactored, for daily challenge).

--- Easy/1971_Find_if_path_exists_in_graph/main.go
+++ Easy/1971_Find_if_path_exists_in_graph/main.go
@@ -1,19 +1,39 @@
 // https://leetcode.com/problems/find-if-path-exists-in-graph/
 package main
 
+type Queue struct {
+	inner      []int
+	start, end int
+}
+
+func MakeQ(capacity int) Queue    { return Queue{inner: make([]int, capacity+1)} }
+func (q Queue) Empty() bool       { return q.start == q.end }
+func (q *Queue) Enqueue(item int) { q.inner[q.end], q.end = item, (q.end+1)%len(q.inner) }
+func (q *Queue) Dequeue() (item int) {
+	item, q.start = q.inner[q.start], (q.start+1)%len(q.inner)
+	return item
+}
+
 func validPath(n int, edges [][]int, source int, destination int) bool {
-	adj := make(map[int][]int)
-	for _, edge := range edges {
-		adj[edge[0]] = append(adj[edge[0]], edge[1])
-		adj[edge[1]] = append(adj[edge[1]], edge[0])
+	if source == destination {
+		return true
 	}
-	for bfs, done, root := []int{source}, map[int]bool{source: true}, 0; len(bfs) > 0; {
-		if root, bfs = bfs[0], bfs[1:]; root == destination {
-			return true
-		}
-		for _, child := range adj[root] {
+	adj := make([][]int, n)
+	for _, e := range edges {
+		adj[e[0]] = append(adj[e[0]], e[1])
+		adj[e[1]] = append(adj[e[1]], e[0])
+	}
+	queue, done := MakeQ(n), make([]bool, n)
+	done[source] = true
+	for queue.Enqueue(source); !queue.Empty(); {
+		node := queue.Dequeue()
+		for _, child := range adj[node] {
+			if child == destination {
+				return true
+			}
 			if !done[child] {
-				done[child], bfs = true, append(bfs, child)
+				queue.Enqueue(child)
+				done[child] = true
 			}
 		}
 	}
