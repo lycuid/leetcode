2330ee63e7158817541838959591c7a667b430f0
Abhishek Kadam <abhishekk19@gmail.com>
2024-07-01 02:34:00 +0530

    Remove max number of edges to keep graph fully traversable (refactored, for daily challenge).

--- Hard/1579_Remove_max_number_of_edges_to_keep_graph_fully_traversable/main.go
+++ Hard/1579_Remove_max_number_of_edges_to_keep_graph_fully_traversable/main.go
@@ -1,59 +1,58 @@
 // https://leetcode.com/problems/remove-max-number-of-edges-to-keep-graph-fully-traversable/
 package main
 
-import (
-	"fmt"
-)
+type Graph struct{ group []int }
 
-type Graph struct {
-	groups int
-	parent []int
-}
-
-func MakeGraph(n int) Graph {
-	parent := make([]int, n)
-	for i := range parent {
-		parent[i] = i
+func MakeGraph(n int) *Graph {
+	group := make([]int, n)
+	for i := range group {
+		group[i] = i
 	}
-	return Graph{n, parent}
+	return &Graph{group}
 }
 
-func (this *Graph) Union(x, y int) (unified int) {
-	if px, py := this.Find(x), this.Find(y); px != py {
-		this.parent[py], this.groups, unified = px, this.groups-1, 1
+func (graph *Graph) Union(x, y int) {
+	if gx, gy := graph.Find(x), graph.Find(y); gx != gy {
+		graph.group[gy] = gx
 	}
-	return unified
 }
 
-func (this *Graph) Find(x int) int {
-	if x != this.parent[x] {
-		this.parent[x] = this.Find(this.parent[x])
+func (graph *Graph) Find(x int) int {
+	if x != graph.group[x] {
+		graph.group[x] = graph.Find(graph.group[x])
 	}
-	return this.parent[x]
+	return graph.group[x]
 }
 
-func maxNumEdgesToRemove(n int, edges [][]int) int {
-	graph, ret := [2]Graph{MakeGraph(n), MakeGraph(n)}, len(edges)
-	for i, j := 0, len(edges)-1; i < j; i++ {
-		if edges[i][0] != 3 {
-			edges[i], edges[j], i, j = edges[j], edges[i], i-1, j-1
+func maxNumEdgesToRemove(n int, edges [][]int) (count int) {
+	graphs := []*Graph{MakeGraph(n + 1), MakeGraph(n + 1)}
+	for _, e := range edges {
+		if e[0] == 3 {
+			if graphs[0].Find(e[1]) == graphs[0].Find(e[2]) {
+				count++
+			} else {
+				graphs[0].Union(e[1], e[2])
+			}
 		}
 	}
-	for _, edge := range edges {
-		if e, u, v := edge[0]-1, edge[1]-1, edge[2]-1; e < 2 {
-			ret -= graph[e].Union(u, v)
-		} else {
-			ret -= graph[0].Union(u, v) | graph[1].Union(u, v)
+	copy(graphs[1].group, graphs[0].group)
+	for _, e := range edges {
+		if e[0] != 3 {
+			if graph := graphs[e[0]-1]; graph.Find(e[1]) == graph.Find(e[2]) {
+				count++
+			} else {
+				graph.Union(e[1], e[2])
+			}
 		}
 	}
-	if graph[0].groups != 1 || graph[1].groups != 1 {
-		return -1
+	for i := 2; i <= n; i++ {
+		for j := 0; j < len(graphs); j++ {
+			if graphs[j].Find(i) != graphs[j].Find(i-1) {
+				return -1
+			}
+		}
 	}
-	return ret
+	return count
 }
 
-func main() {
-	fmt.Println(maxNumEdgesToRemove(4, [][]int{{3, 1, 2}, {3, 2, 3}, {1, 1, 3}, {1, 2, 4}, {1, 1, 2}, {2, 3, 4}}))
-	fmt.Println(maxNumEdgesToRemove(4, [][]int{{3, 1, 2}, {3, 2, 3}, {1, 1, 4}, {2, 1, 4}}))
-	fmt.Println(maxNumEdgesToRemove(4, [][]int{{3, 1, 2}, {3, 2, 3}, {1, 1, 4}}))
-}
+func main() {}
