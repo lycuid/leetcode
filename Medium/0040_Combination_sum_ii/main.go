c4eae7471bbed50f78f2af87e13ccce9c3160347
Abhishek Kadam <abhishekk19@gmail.com>
2024-08-13 19:34:14 +0530

    Combination sum ii (refactored, for daily challenge).

--- Medium/0040_Combination_sum_ii/main.go
+++ Medium/0040_Combination_sum_ii/main.go
@@ -4,22 +4,23 @@ package main
 import "sort"
 
 func Aux(nums []int, target int) (ret [][]int) {
-	if target >= 0 {
-		if target == 0 {
-			return [][]int{nil}
+	if target == 0 {
+		return [][]int{nil}
+	}
+	if len(nums) == 0 || nums[0] > target {
+		return nil
+	}
+	for i := 0; i < len(nums); {
+		for _, val := range Aux(nums[i+1:], target-nums[i]) {
+			ret = append(ret, append([]int{nums[i]}, val...))
 		}
-		for i := 0; i < len(nums); {
-			for _, comb := range Aux(nums[i+1:], target-nums[i]) {
-				ret = append(ret, append([]int{nums[i]}, comb...))
-			}
-			for i++; i < len(nums) && nums[i-1] == nums[i]; i++ {
-			}
+		for i++; i < len(nums) && nums[i-1] == nums[i]; i++ {
 		}
 	}
 	return ret
 }
 
-func combinationSum2(candidates []int, target int) [][]int {
+func combinationSum2(candidates []int, target int) (ret [][]int) {
 	sort.Ints(candidates)
 	return Aux(candidates, target)
 }
