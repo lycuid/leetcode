46c63d9ecf4dbc5bb0aad1e6594597aa9207f599
Abhishek Kadam <abhishekk19@gmail.com>
2025-05-05 19:38:59 +0530

    Domino and tromino tiling (refactored, for daily challenge).

--- Medium/0790_Domino_and_tromino_tiling/main.go
+++ Medium/0790_Domino_and_tromino_tiling/main.go
@@ -2,12 +2,12 @@
 package main
 
 func numTilings(n int) int {
-	dom, trom, i, MOD := make([]int, n+2), make([]int, n+2), 3, int(1e9+7)
-	for dom[0], dom[1], dom[2], trom[2] = 1, 1, 2, 1; i <= n; i++ {
-		dom[i] = (dom[i-1] + dom[i-2] + trom[i-1]*2) % MOD
-		trom[i] = (dom[i-2] + trom[i-1]) % MOD
+	cache := make([]int, n+3)
+	cache[0], cache[1], cache[2] = 1, 1, 2
+	for i := 3; i <= n; i++ {
+		cache[i] = (2*cache[i-1] + cache[i-3]) % (1e9 + 7)
 	}
-	return dom[n] % MOD
+	return cache[n]
 }
 
 func main() {}
