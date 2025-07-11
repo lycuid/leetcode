57789bda6444350be002755d0af2ccfaff485dd0
Abhishek Kadam <abhishekk19@gmail.com>
2025-07-12 03:16:19 +0530

    Meeting rooms iii (refactored, for daily challenge).

--- Hard/2402_Meeting_rooms_iii/main.go
+++ Hard/2402_Meeting_rooms_iii/main.go
@@ -3,91 +3,91 @@ package main
 
 import "sort"
 
-type Comparable[T any] interface{ Less(T) bool }
-type Room uint64
-type Tuple struct{ room, start, end uint64 }
-
-func (this Room) Less(that Room) bool   { return this < that }
-func (this Tuple) Less(that Tuple) bool {
-	if this.end == that.end {
-		return this.room < that.room
-	}
-	return this.end < that.end
+type priorityQueue struct {
+	items []any
+	less  func(any, any) bool
 }
 
-type PriorityQueue[T Comparable[T]] struct{ inner []T }
-
-func MakePQ[T Comparable[T]]() PriorityQueue[T] {
-	return PriorityQueue[T]{make([]T, 1)}
+func makePQ(less func(any, any) bool) priorityQueue {
+	return priorityQueue{make([]any, 1), less}
 }
 
-func (pq PriorityQueue[T]) Top() (item *T) {
-	if len(pq.inner) > 1 {
-		item = &pq.inner[1]
-	}
-	return item
-}
-
-func (pq *PriorityQueue[T]) Push(item T) {
-	pq.inner = append(pq.inner, item)
-	for i := len(pq.inner) - 1; i > 1 && pq.inner[i].Less(pq.inner[i/2]); i /= 2 {
-		pq.inner[i], pq.inner[i/2] = pq.inner[i/2], pq.inner[i]
+func (pq *priorityQueue) push(item any) {
+	pq.items = append(pq.items, item)
+	for i := len(pq.items) - 1; i > 1 && pq.less(pq.items[i], pq.items[i/2]); i /= 2 {
+		pq.items[i], pq.items[i/2] = pq.items[i/2], pq.items[i]
 	}
 }
 
-func (pq *PriorityQueue[T]) Pop() T {
-	item, n := pq.inner[1], len(pq.inner)-1
-	pq.inner[1], pq.inner = pq.inner[n], pq.inner[:n]
+func (pq *priorityQueue) pop() any {
+	item, n := pq.items[1], len(pq.items)-1
+	pq.items[1], pq.items = pq.items[n], pq.items[:n]
 	for i, j := 1, 2; j < n; i, j = j, j*2 {
-		if j+1 < n && pq.inner[j+1].Less(pq.inner[j]) {
+		if j+1 < n && pq.less(pq.items[j+1], pq.items[j]) {
 			j++
 		}
-		if pq.inner[i].Less(pq.inner[j]) {
+		if pq.less(pq.items[i], pq.items[j]) {
 			break
 		}
-		pq.inner[i], pq.inner[j] = pq.inner[j], pq.inner[i]
+		pq.items[i], pq.items[j] = pq.items[j], pq.items[i]
 	}
 	return item
 }
 
-func mostBooked(n int, meetings [][]int) (max int) {
-	if n > 1 && len(meetings) > 1 {
-		cost, meets, rooms := make([]int, n), MakePQ[Tuple](), MakePQ[Room]()
+func (pq priorityQueue) top() *any {
+	if len(pq.items) == 1 {
+		return nil
+	}
+	return &pq.items[1]
+}
+
+func mostBooked(n int, meetings [][]int) (room int) {
+	if len(meetings) > 0 {
+		score := make([]int, n)
+
+		type Slot struct{ room, end int }
+		slots := makePQ(func(i, j any) bool {
+			if i.(Slot).end == j.(Slot).end {
+				return i.(Slot).room < j.(Slot).room
+			}
+			return i.(Slot).end < j.(Slot).end
+		})
+
+		rooms := makePQ(func(i, j any) bool {
+			return i.(int) < j.(int)
+		})
 		for i := 0; i < n; i++ {
-			rooms.Push(Room(i))
+			rooms.push(i)
 		}
+
 		sort.Slice(meetings, func(i, j int) bool {
 			return meetings[i][0] < meetings[j][0]
 		})
+
 		for _, meeting := range meetings {
-			for meets.Top() != nil && meets.Top().end <= uint64(meeting[0]) {
-				rooms.Push(Room(meets.Pop().room))
+			for slots.top() != nil && (*slots.top()).(Slot).end <= meeting[0] {
+				rooms.push(slots.pop().(Slot).room)
 			}
-			var curr Tuple
-			if rooms.Top() != nil {
-				curr = Tuple{
-					room:  uint64(int(rooms.Pop())),
-					start: uint64(meeting[0]),
-					end:   uint64(meeting[1]),
-				}
+
+			var slot Slot
+			if rooms.top() != nil {
+				slot.room = rooms.pop().(int)
 			} else {
-				prev := meets.Pop()
-				curr = Tuple{
-					room:  prev.room,
-					start: prev.end,
-					end:   uint64(meeting[1]) + prev.end - uint64(meeting[0]),
-				}
+				slot = slots.pop().(Slot)
+				meeting[1] += slot.end - meeting[0]
 			}
-			cost[curr.room]++
-			meets.Push(curr)
+			slot.end = meeting[1]
+			score[slot.room]++
+			slots.push(slot)
 		}
-		for i := len(cost) - 1; i >= 0; i-- {
-			if cost[i] >= cost[max] {
-				max = i
+
+		for i := range score {
+			if score[i] > score[room] {
+				room = i
 			}
 		}
 	}
-	return max
+	return room
 }
 
 func main() {}
