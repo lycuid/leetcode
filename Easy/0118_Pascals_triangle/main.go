1b76c842f6417fe75ec41b0dfa679a6a64703b6b
Abhishek Kadam <abhishekk19@gmail.com>
2025-08-01 16:04:57 +0530

    Pascal's triangle (refactored, for daily challenge).

--- Easy/0118_Pascals_triangle/main.go
+++ Easy/0118_Pascals_triangle/main.go
@@ -1,16 +1,19 @@
 // https://leetcode.com/problems/pascals-triangle/
 package main
 
-func generate(num int) [][]int {
-	rows := make([][]int, num)
-	for i := 0; i < num; i++ {
-		rows[i] = make([]int, i+1)
-		rows[i][0], rows[i][i] = 1, 1
-		for j := 1; j < i; j++ {
-			rows[i][j] = rows[i-1][j-1] + rows[i-1][j]
+func generate(numRows int) [][]int {
+	ret := make([][]int, 0, numRows)
+	ret = append(ret, []int{1})
+	for i := 1; i < numRows; i++ {
+		tmp := make([]int, 0, len(ret[i-1])+2)
+		tmp = append(tmp, 1)
+		for j := 0; j < len(ret[i-1])-1; j++ {
+			tmp = append(tmp, ret[i-1][j]+ret[i-1][j+1])
 		}
+		tmp = append(tmp, 1)
+		ret = append(ret, tmp)
 	}
-	return rows
+	return ret
 }
 
 func main() {}
