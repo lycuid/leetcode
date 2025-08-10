5df115465b84f0eb9b7a6b8c8bf471f6b450e305
Abhishek Kadam <abhishekk19@gmail.com>
2025-08-10 17:11:20 +0530

    Reordered power of 2 (refactored, for daily challenge).

--- Medium/0869_Reordered_power_of_2/main.go
+++ Medium/0869_Reordered_power_of_2/main.go
@@ -1,39 +1,30 @@
 // https://leetcode.com/problems/reordered-power-of-2/
 package main
 
-import "fmt"
-
-const UPPER_BOUND int = 1e9
-
-func MatchesDigitCount(num1, num2 int) bool {
-	var freq [10]int
-	for i := num1; i > 0; i /= 10 {
-		freq[i%10]++
-	}
-	for i := num2; i > 0; i /= 10 {
-		if freq[i%10]--; freq[i%10] < 0 {
-			break
-		}
-	}
-	for _, f := range freq {
-		if f != 0 {
-			return false
-		}
-	}
-	return true
-}
+import (
+	"slices"
+	"sort"
+)
 
 func reorderedPowerOf2(n int) bool {
-	for i := 0; 1<<i < UPPER_BOUND; i++ {
-		if MatchesDigitCount(n, 1<<i) {
+	digits := func(n int) (d []int) {
+		for ; n > 0; n /= 10 {
+			d = append(d, n%10)
+		}
+		sort.Ints(d)
+		return d
+	}
+	numDigits := digits(n)
+	for i := 0; ; i++ {
+		finalDigits := digits(1 << i)
+		if len(finalDigits) > len(numDigits) {
+			break
+		}
+		if eq := slices.Equal(finalDigits, numDigits); eq {
 			return true
 		}
 	}
 	return false
 }
 
-func main() {
-	fmt.Println(reorderedPowerOf2(1))
-	fmt.Println(reorderedPowerOf2(10))
-	fmt.Println(reorderedPowerOf2(1024))
-}
+func main() {}
