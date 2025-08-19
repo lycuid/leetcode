e3c124cda6e038d964dac6ddb5c62202e76098dc
Abhishek Kadam <abhishekk19@gmail.com>
2025-08-19 11:30:03 +0530

    Number of zero filled subarrays (refactored, for daily challenge).

--- Medium/2348_Number_of_zero_filled_subarrays/main.go
+++ Medium/2348_Number_of_zero_filled_subarrays/main.go
@@ -2,12 +2,14 @@
 package main
 
 func zeroFilledSubarray(nums []int) (ret int64) {
-	for i, j := 0, 0; i < len(nums); i = j {
-		for ; i < len(nums) && nums[i] != 0; i++ {
+	var count int64
+	for _, num := range nums {
+		if num == 0 {
+			count++
+		} else {
+			count = 0
 		}
-		for j = i; j < len(nums) && nums[j] == 0; j++ {
-		}
-		ret += int64(((j - i) * ((j - i) + 1)) / 2)
+		ret += count
 	}
 	return ret
 }
