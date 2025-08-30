88e13f5651d9607101af7b4232017dc4b00a7ebf
Abhishek Kadam <abhishekk19@gmail.com>
2025-08-31 03:03:58 +0530

    Valid sudoku (refactored, for daily challenge).

--- Medium/0036_Valid_sudoku/main.go
+++ Medium/0036_Valid_sudoku/main.go
@@ -2,26 +2,22 @@
 package main
 
 func isValidSudoku(board [][]byte) bool {
-	for i := 0; i < 9; i++ {
-		var horizontal, vertical, box [9]bool
-		for j := 0; j < 9; j++ {
-			if hy, hx := i, j; board[hy][hx] != '.' {
-				if horizontal[board[hy][hx]-'1'] {
-					return false
+	for i := range board {
+		for j := range board[i] {
+			if ch := board[i][j]; ch != '.' {
+				board[i][j] = '.'
+				for k := 0; k < 9; k++ {
+					if board[i][k] == ch {
+						return false
+					}
+					if board[k][j] == ch {
+						return false
+					}
+					if board[i/3*3+(k/3)][j/3*3+(k%3)] == ch {
+						return false
+					}
 				}
-				horizontal[board[hy][hx]-'1'] = true
-			}
-			if vy, vx := j, i; board[vy][vx] != '.' {
-				if vertical[board[vy][vx]-'1'] {
-					return false
-				}
-				vertical[board[vy][vx]-'1'] = true
-			}
-			if by, bx := ((i%3)*3)+(j/3), ((i/3)*3)+(j%3); board[by][bx] != '.' {
-				if box[board[by][bx]-'1'] {
-					return false
-				}
-				box[board[by][bx]-'1'] = true
+				board[i][j] = ch
 			}
 		}
 	}
