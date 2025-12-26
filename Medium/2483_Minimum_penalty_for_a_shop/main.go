31f8ec2aef27f995d06f44a753a2cb79fbbe4f82
Abhishek Kadam <abhishekk19@gmail.com>
2025-12-27 03:51:49 +0530

    Minimum penalty for a shop (refactored, for daily challenge).

--- Medium/2483_Minimum_penalty_for_a_shop/main.go
+++ Medium/2483_Minimum_penalty_for_a_shop/main.go
@@ -1,23 +1,23 @@
 // https://leetcode.com/problems/minimum-penalty-for-a-shop/
 package main
 
-func bestClosingTime(customers string) (ret int) {
-	post := make([]int, len(customers)+1)
+func bestClosingTime(customers string) (hour int) {
+	right, left, lowest := make([]int, len(customers)+1), 0, len(customers)
 	for i := len(customers) - 1; i >= 0; i-- {
-		if post[i] = post[i+1]; customers[i] == 'Y' {
-			post[i]++
+		if right[i] = right[i+1]; customers[i] == 'Y' {
+			right[i]++
 		}
 	}
-	pre, penalty := 0, post[0]
-	for i := 1; i < len(post); i++ {
-		if customers[i-1] == 'N' {
-			pre++
+	customers += " "
+	for i, val := range customers {
+		if penalty := left + right[i]; penalty < lowest {
+			lowest, hour = penalty, i
 		}
-		if n := pre + post[i]; n < penalty {
-			penalty, ret = n, i
+		if val == 'N' {
+			left++
 		}
 	}
-	return ret
+	return hour
 }
 
 func main() {}
