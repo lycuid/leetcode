c7bde2142a2d09373645917bab4e3347c92641c0
Abhishek Kadam <abhishekk19@gmail.com>
2025-12-29 02:04:07 +0530

    Count negative numbers in a sorted matrix (refactored, for daily challenge).

--- Easy/1351_Count_negative_numbers_in_a_sorted_matrix/main.go
+++ Easy/1351_Count_negative_numbers_in_a_sorted_matrix/main.go
@@ -1,19 +1,19 @@
 // https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
 package main
 
-func countNegatives(grid [][]int) (ret int) {
-	for i, r := 0, len(grid[0]); i < len(grid); i++ {
-		l := 0
+func countNegatives(grid [][]int) (count int) {
+	for _, row := range grid {
+		l, r := 0, len(row)
 		for l < r {
-			if mid := (l + r) / 2; grid[i][mid] < 0 {
+			if mid := l + (r-l)/2; row[mid] < 0 {
 				r = mid
 			} else {
 				l = mid + 1
 			}
 		}
-		ret += len(grid[i]) - l
+		count += len(row) - l
 	}
-	return ret
+	return count
 }
 
 func main() {}
