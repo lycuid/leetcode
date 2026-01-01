cde7a5ccdaee4ef0c6d8dd7450c27443d3ef6b65
Abhishek Kadam <abhishekk19@gmail.com>
2026-01-01 21:34:18 +0530

    Plus one (refactored, for daily challenge).

--- Easy/0066_Plus_one/main.go
+++ Easy/0066_Plus_one/main.go
@@ -1,21 +1,21 @@
 // https://leetcode.com/problems/plus-one/
-
 package main
 
 func plusOne(digits []int) []int {
-  carry := 1
-  i := len(digits) - 1
-  for i >= 0 && carry > 0 {
-    digits[i] += carry
-    carry = int(digits[i] / 10)
-    digits[i] %= 10
-    i--
-  }
-  if carry > 0 {
-    digits = append([]int{carry}, digits...)
-  }
-  return digits
+	for i := 0; i < len(digits)/2; i++ {
+		digits[i], digits[len(digits)-1-i] = digits[len(digits)-1-i], digits[i]
+	}
+	for carry, i := 1, 0; carry > 0; i++ {
+		if i >= len(digits) {
+			digits = append(digits, 0)
+		}
+		carry += digits[i]
+		digits[i], carry = carry%10, carry/10
+	}
+	for i := 0; i < len(digits)/2; i++ {
+		digits[i], digits[len(digits)-1-i] = digits[len(digits)-1-i], digits[i]
+	}
+	return digits
 }
 
-func main() { }
-
+func main() {}
