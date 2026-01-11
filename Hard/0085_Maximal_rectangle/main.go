1b6356e2d170ef35e9eddf65327899afcad1ffd6
Abhishek Kadam <abhishekk19@gmail.com>
2026-01-12 02:28:52 +0530

    Maximal rectangle (refactored, for daily challenge).

--- Hard/0085_Maximal_rectangle/main.go
+++ Hard/0085_Maximal_rectangle/main.go
@@ -1,32 +1,30 @@
 // https://leetcode.com/problems/maximal-rectangle/
 package main
 
-func maximalRectangle(matrix [][]byte) (ret int) {
-	cache := make([][]int, len(matrix))
-	for i := range cache {
-		cache[i] = make([]int, len(matrix[i]))
-		if len(cache[i]) > 0 {
-			cache[i][0] = int(matrix[i][0] - '0')
-		}
-		for j := 1; j < len(cache[i]); j++ {
-			if matrix[i][j] != '0' {
-				cache[i][j] = cache[i][j-1] + int(matrix[i][j]-'0')
+func maximalRectangle(matrix [][]byte) (area int) {
+	if len(matrix) > 0 {
+		for j := range matrix[0] {
+			matrix[0][j] -= '0'
+			for i := 1; i < len(matrix); i++ {
+				matrix[i][j] = (matrix[i-1][j] + 1) * (matrix[i][j] - '0')
 			}
 		}
-	}
-	for i := range cache {
-		for j := range cache[i] {
-			for k, min := 0, cache[i][j]; k <= i; k++ {
-				if min > cache[i-k][j] {
-					min = cache[i-k][j]
+		s := make([]int, 1, len(matrix[0])+1)
+		s[0] = -1
+		for i := range matrix {
+			for j := range matrix[i] {
+				for ; len(s) > 1 && matrix[i][s[len(s)-1]] >= matrix[i][j]; s = s[:len(s)-1] {
+					area = max(area, int(matrix[i][s[len(s)-1]])*(j-s[len(s)-2]-1))
 				}
-				if n := (k + 1) * min; n > ret {
-					ret = n
-				}
+				s = append(s, j)
 			}
+			for ; len(s) > 1 && matrix[i][s[len(s)-1]] >= byte(0); s = s[:len(s)-1] {
+				area = max(area, int(matrix[i][s[len(s)-1]])*(len(matrix[i])-s[len(s)-2]-1))
+			}
+			s = s[:1]
 		}
 	}
-	return ret
+	return area
 }
 
 func main() {}
