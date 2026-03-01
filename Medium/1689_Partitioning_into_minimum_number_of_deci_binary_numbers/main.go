0a6881a649bc34e105cb010424126b2ac9df9f60
Abhishek Kadam <abhishekk19@gmail.com>
2026-03-02 03:28:44 +0530

    Partitioning into minimum number of deci-binary numbers (refactored, for daily challenge).

--- Medium/1689_Partitioning_into_minimum_number_of_deci_binary_numbers/main.go
+++ Medium/1689_Partitioning_into_minimum_number_of_deci_binary_numbers/main.go
@@ -1,14 +1,11 @@
 // https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
 package main
 
-func minPartitions(n string) int {
-	ret := n[0]
-	for i := 1; i < len(n); i++ {
-		if n[i] > ret {
-			ret = n[i]
-		}
+func minPartitions(n string) (num int) {
+	for i := range n {
+		num = max(num, int(n[i]-'0'))
 	}
-	return int(ret - '0')
+	return num
 }
 
 func main() {}
