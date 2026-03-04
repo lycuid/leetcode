ea51fa0ec274296b5977a2b618e548b52d3e49ff
Abhishek Kadam <abhishekk19@gmail.com>
2026-03-05 04:56:57 +0530

    Special positions in a binary matrix (refactored, for daily challenge).

--- Easy/1582_Special_positions_in_a_binary_matrix/main.go
+++ Easy/1582_Special_positions_in_a_binary_matrix/main.go
@@ -2,15 +2,18 @@
 package main
 
 func numSpecial(mat [][]int) (count int) {
-	rows, cols := make([]int, len(mat)), make([]int, len(mat[0]))
+	var cache [2][]int
+	cache[0] = make([]int, len(mat))
+	cache[1] = make([]int, len(mat[0]))
 	for i := range mat {
 		for j := range mat[i] {
-			rows[i], cols[j] = rows[i]+mat[i][j], cols[j]+mat[i][j]
+			cache[0][i] += mat[i][j]
+			cache[1][j] += mat[i][j]
 		}
 	}
 	for i := range mat {
 		for j := range mat[i] {
-			if mat[i][j] == 1 && rows[i] == 1 && cols[j] == 1 {
+			if mat[i][j] == 1 && cache[0][i] == 1 && cache[1][j] == 1 {
 				count++
 			}
 		}
