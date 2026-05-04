da119c189c1ec4880e81a95a44fe2edf3bcf43e8
Abhishek Kadam <abhishekk19@gmail.com>
2026-05-04 22:09:26 +0530

    Rotate image.

--- Medium/0048_Rotate_image/main.go
+++ Medium/0048_Rotate_image/main.go
@@ -1,12 +1,13 @@
 // https://leetcode.com/problems/rotate-image/
 package main
 
-func rotate(m [][]int) {
-	n := len(m) - 1
+func rotate(matrix [][]int) {
+	n := len(matrix) - 1
 	for i := 0; i <= n/2; i++ {
-		for j := i; j < n-i; j++ {
-			m[i][j], m[j][n-i], m[n-i][n-j], m[n-j][i] =
-				m[n-j][i], m[i][j], m[j][n-i], m[n-i][n-j]
+		for j := i; j <= n-i-1; j++ {
+			matrix[i][j], matrix[n-j][i], matrix[n-i][n-j], matrix[j][n-i] = matrix[n-j][i], matrix[n-i][n-j], matrix[j][n-i], matrix[i][j]
 		}
 	}
 }
+
+func main() {}
