0424aa9490a188f496a4e53b266057c57ad2e7a2
Abhishek Kadam <abhishekk19@gmail.com>
2026-05-18 01:28:36 +0530

    Jump game iii (refactored, for daily challenge).

--- Medium/1306_Jump_game_iii/main.go
+++ Medium/1306_Jump_game_iii/main.go
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/jump-game-iii/
 package main
 
-func Solve(start int, nums []int, visited []bool) bool {
+func find(start int, nums []int, visited []bool) bool {
 	if start < 0 || start >= len(nums) || visited[start] {
 		return false
 	}
@@ -9,13 +9,14 @@ func Solve(start int, nums []int, visited []bool) bool {
 		return true
 	}
 	visited[start] = true
-	left, right := Solve(start-nums[start], nums, visited), Solve(start+nums[start], nums, visited)
+	l := find(start-nums[start], nums, visited)
+	r := find(start+nums[start], nums, visited)
 	visited[start] = false
-	return left || right
+	return l || r
 }
 
-func canReach(nums []int, start int) bool {
-	return Solve(start, nums, make([]bool, len(nums)))
+func canReach(arr []int, start int) bool {
+	return find(start, arr, make([]bool, len(arr)))
 }
 
 func main() {}
