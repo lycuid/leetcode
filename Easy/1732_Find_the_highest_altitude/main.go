a26c33f8374fd28d9f61ffbdbaf388f72a0f6bd4
Abhishek Kadam <abhishekk19@gmail.com>
2026-06-19 20:14:24 +0530

    Find the highest altitude (refactored, for daily challenge).

--- Easy/1732_Find_the_highest_altitude/main.go
+++ Easy/1732_Find_the_highest_altitude/main.go
@@ -1,13 +1,12 @@
 // https://leetcode.com/problems/find-the-highest-altitude/
 package main
 
-func largestAltitude(gain []int) (ret int) {
-	for i, altitude := 0, 0; i < len(gain); i++ {
-		if altitude += gain[i]; altitude > ret {
-			ret = altitude
-		}
+func largestAltitude(gain []int) (res int) {
+	for i, alt := 0, 0; i < len(gain); i++ {
+		alt += gain[i]
+		res = max(res, alt)
 	}
-	return ret
+	return res
 }
 
 func main() {}
