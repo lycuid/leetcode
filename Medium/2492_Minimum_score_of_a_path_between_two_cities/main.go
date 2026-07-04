807adb3ddb91deeca0eac80e4dca88a224d4af5a
Abhishek Kadam <abhishekk19@gmail.com>
2026-07-05 05:20:48 +0530

    Minimum score of a path between two cities (refactored, for daily challenge).

--- Medium/2492_Minimum_score_of_a_path_between_two_cities/main.go
+++ Medium/2492_Minimum_score_of_a_path_between_two_cities/main.go
@@ -3,34 +3,50 @@ package main
 
 import "math"
 
-func Aux(src, dst int, edges map[int]map[int]int, visited map[int]bool) (ret int) {
-	if ret = math.MaxInt; !visited[src] {
-		visited[src] = true
-		for child, distance := range edges[src] {
-			if distance < ret {
-				ret = distance
-			}
-			if n := Aux(child, dst, edges, visited); n < ret {
-				ret = n
-			}
+type Graph struct{ parent []int }
+
+func makeGraph(n int) Graph {
+	parent := make([]int, n)
+	for i := range parent {
+		parent[i] = i
+	}
+	return Graph{parent}
+}
+
+func (g *Graph) union(x, y int) {
+	if px, py := g.find(x), g.find(y); px != py {
+		if px > py {
+			g.parent[py] = px
+		} else {
+			g.parent[px] = py
 		}
 	}
-	return ret
+}
+
+func (g *Graph) find(x int) int {
+	if x != g.parent[x] {
+		g.parent[x] = g.find(g.parent[x])
+	}
+	return g.parent[x]
 }
 
 func minScore(n int, roads [][]int) int {
-	edges := make(map[int]map[int]int)
-	for _, e := range roads {
-		from, to, distance := e[0], e[1], e[2]
-		if edges[from] == nil {
-			edges[from] = make(map[int]int)
-		}
-		if edges[to] == nil {
-			edges[to] = make(map[int]int)
-		}
-		edges[from][to], edges[to][from] = distance, distance
+	graph := makeGraph(n + 1)
+	cost := make([]int, n+1)
+	for i := range cost {
+		cost[i] = math.MaxInt
 	}
-	return Aux(1, n, edges, make(map[int]bool))
+	for _, edge := range roads {
+		graph.union(edge[0], edge[1])
+		cost[edge[0]] = min(cost[edge[0]], edge[2])
+		cost[edge[1]] = min(cost[edge[1]], edge[2])
+	}
+	for i := 0; i < n; i++ {
+		if graph.find(i) == n {
+			cost[n] = min(cost[n], cost[i])
+		}
+	}
+	return cost[n]
 }
 
 func main() {}
