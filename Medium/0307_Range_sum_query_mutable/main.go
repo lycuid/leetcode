c4c3e12014861a2323afc57ab665376e775ba557
Abhishek Kadam <abhishekk19@gmail.com>
2026-07-11 13:43:55 +0530

    Range sum query - mutable (refactored).

--- Medium/0307_Range_sum_query_mutable/main.go
+++ Medium/0307_Range_sum_query_mutable/main.go
@@ -1,23 +1,44 @@
 // https://leetcode.com/problems/range-sum-query-mutable/
 package main
 
-type NumArray struct {
-	inner []int
-}
+type NumArray struct{ tree []int }
 
 func Constructor(nums []int) NumArray {
-	return NumArray{nums}
+	n := len(nums)
+	for n&(n-1) != 0 {
+		n++
+	}
+	tree := make([]int, 2*n)
+	for i := range nums {
+		tree[n+i] = nums[i]
+	}
+	for i := n - 1; i >= 1; i-- {
+		tree[i] = tree[i*2] + tree[i*2+1]
+	}
+	return NumArray{tree}
 }
 
 func (this *NumArray) Update(index int, val int) {
-	this.inner[index] = val
+	index += (len(this.tree) / 2)
+	this.tree[index] = val
+	for index /= 2; index >= 1; index /= 2 {
+		this.tree[index] = this.tree[index*2] + this.tree[index*2+1]
+	}
 }
 
-func (this *NumArray) SumRange(left int, right int) (ret int) {
-	for i := left; i <= right; i++ {
-		ret += this.inner[i]
+func (this *NumArray) SumRange(left int, right int) int {
+	var q func(int, int, int) int
+	q = func(node, low, high int) int {
+		if low >= left && high <= right {
+			return this.tree[node]
+		}
+		if low > right || high < left {
+			return 0
+		}
+		mid := (low + high) / 2
+		return q(node*2, low, mid) + q(node*2+1, mid+1, high)
 	}
-	return ret
+	return q(1, 0, len(this.tree)/2-1)
 }
 
 func main() {}
