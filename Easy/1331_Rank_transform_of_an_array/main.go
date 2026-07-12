5c0d9c618529436e7abea99245ef18e96667b6c2
Abhishek Kadam <abhishekk19@gmail.com>
2026-07-12 10:04:45 +0530

    Rank transform of an array (refactored, for daily challenge).

--- Easy/1331_Rank_transform_of_an_array/main.go
+++ Easy/1331_Rank_transform_of_an_array/main.go
@@ -3,28 +3,25 @@ package main
 
 import "sort"
 
-func arrayRankTransform(arr []int) (rank []int) {
+func arrayRankTransform(arr []int) (res []int) {
 	if n := len(arr); n > 0 {
-		rank, indices := make([]int, n), make([]int, n)
+		indices := make([]int, n)
 		for i := range indices {
 			indices[i] = i
 		}
 		sort.Slice(indices, func(i, j int) bool {
-			if arr[indices[i]] == arr[indices[j]] {
-				return indices[i] < indices[j]
-			}
 			return arr[indices[i]] < arr[indices[j]]
 		})
-		rank[indices[0]] = 1
+		res = make([]int, n)
+		res[indices[0]] = 1
 		for i := 1; i < n; i++ {
-			rank[indices[i]] = rank[indices[i-1]]
+			res[indices[i]] = res[indices[i-1]]
 			if arr[indices[i]] != arr[indices[i-1]] {
-				rank[indices[i]]++
+				res[indices[i]]++
 			}
 		}
-		return rank
 	}
-	return nil
+	return res
 }
 
 func main() {}
