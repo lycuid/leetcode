1a4dd2301249a5f0156525d4e8bb13a5cd78a8d3
Abhishek Kadam <abhishekk19@gmail.com>
2026-07-15 07:52:24 +0530

    GCD of odd and even sums.

---
+++ Easy/3658_Gcd_of_odd_and_even_sums/main.go
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+package main
+
+func gcdOfOddEvenSums(n int) int {
+	// even sum -> n * (n + 1)
+	// odd  sum -> n * n
+	// gcd(n*(n+1), n*n) -> n
+	return n
+}
+
+func main() {}
