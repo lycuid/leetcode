6f3fe768ad68ae12707dc5e75b997e4fe43fcd1c
Abhishek Kadam <abhishekk19@gmail.com>
2026-07-27 10:04:44 +0530

    Maximum product of two elements in an array (refactored, for daily challenge).

--- Easy/1464_Maximum_product_of_two_elements_in_an_array/main.go
+++ Easy/1464_Maximum_product_of_two_elements_in_an_array/main.go
@@ -1,11 +1,18 @@
 // https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
 package main
 
-import "sort"
-
 func maxProduct(nums []int) int {
-	sort.Ints(nums)
-	return (nums[len(nums)-1] - 1) * (nums[len(nums)-2] - 1)
+	if nums[0] < nums[1] {
+		nums[0], nums[1] = nums[1], nums[0]
+	}
+	for _, num := range nums[2:] {
+		if num > nums[0] {
+			nums[0], nums[1] = num, nums[0]
+		} else if num > nums[1] {
+			nums[1] = num
+		}
+	}
+	return (nums[0] - 1) * (nums[1] - 1)
 }
 
 func main() {}
